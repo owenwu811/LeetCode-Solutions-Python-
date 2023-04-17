5777eff03879b1617388c310b036601d3c3f3878
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-04-16 23:30:44 -0400

    Update foursum.py
    My Solution (Python)

--- foursum.py
+++ foursum.py
@@ -30,13 +30,13 @@ class Solution(object):
     def fourSum(self, nums, target):
         result = []
         nums.sort()
-        for first in range(len(nums) - 3): #index 0 to not including 1 if len(nums) == 4
+        for first in range(len(nums) - 3): #first pointer increments when second pointer is violating the len(nums) - 2 condition, meaning no more room for a 4th pointer to the right
             if first > 0 and nums[first] == nums[first - 1]:
                 continue
-            for second in range(first + 1, len(nums) - 2): #index 1 to not including 2 if len(nums) == 4
+            for second in range(first + 1, len(nums) - 2): #second pointer increments when while third < fourth is false
                 if second > first + 1 and nums[second] == nums[second - 1]:
                     continue
-                third = second + 1
+                third = second + 1 #note that third and fourth pointer values are set according to their relationship to first and second pointer; current third and fourh pointer values have no relationship to what the previous third and fourth pointer values were before
                 fourth = len(nums) - 1
                 while third < fourth: #we are reducing the problem into a two sum wih two pointer wih the 3rd and 4th pointer, so we need to make sure the third and fourth don't cross, and if they do cross, then go back to 2nd pointer
                     foursum = nums[first] + nums[second] + nums[third] + nums[fourth]
@@ -48,8 +48,10 @@ class Solution(object):
                         result.append([nums[first], nums[second], nums[third], nums[fourth]])
                         third += 1
                         fourth -= 1
-                        while nums[third] == nums[third - 1] and third < fourth: #duplicate checking 
+                        while nums[third] == nums[third - 1] and third < fourth: #even after finding a valid quadruplet, because we just moved third and fourth pointers closer to one another, hence to new elements, we have to duplicate check again to ensure that these new elements are not a repeat of the outer third and fourth pointers that we just counted as a valid quadruplet 
                             third += 1
-                        while nums[fourth] == nums[fourth + 1] and third < fourth:
+                        while nums[fourth] == nums[fourth + 1] and third < fourth: #if this is false, then we go back to while third < fourth check, and if that's false, then we increment second by hitting line 36, and if second dosen't make room for a 4th pointer, then we increment first pointer by hitting line 33
                             fourth -= 1
         return result
+       
+      
