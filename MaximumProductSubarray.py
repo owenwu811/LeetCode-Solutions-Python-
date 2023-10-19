46020c044742c2cc06ce83302ac0de77cd36be24
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-10-19 19:14:12 -0400

    Update MaximumProductSubarray.py

--- MaximumProductSubarray.py
+++ MaximumProductSubarray.py
@@ -1,37 +1,30 @@
-152. Maximum Product Subarray
-Medium
-17.5K
-549
-Companies
-Given an integer array nums, find a 
-subarray
- that has the largest product, and return the product.
+#152. Maximum Product Subarray
+#Given an integer array nums, find a 
+#subarray that has the largest product, and return the product.
+#The test cases are generated so that the answer will fit in a 32-bit integer.
 
-The test cases are generated so that the answer will fit in a 32-bit integer.
+#Example 1:
 
-Example 1:
+#Input: nums = [2,3,-2,4]
+#Output: 6
+#Explanation: [2,3] has the largest product 6.
+#Example 2:
 
-Input: nums = [2,3,-2,4]
-Output: 6
-Explanation: [2,3] has the largest product 6.
-Example 2:
-
-Input: nums = [-2,0,-1]
-Output: 0
-Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+#Input: nums = [-2,0,-1]
+#Output: 0
+#Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
  
+#Constraints:
 
-Constraints:
-
-1 <= nums.length <= 2 * 104
--10 <= nums[i] <= 10
-The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
-Accepted
-1.1M
-Submissions
-3.2M
-Acceptance Rate
-34.8%
+#1 <= nums.length <= 2 * 104
+#-10 <= nums[i] <= 10
+#The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+#Accepted
+#1.1M
+#Submissions
+#3.2M
+#Acceptance Rate
+#34.8%
 
 #correct anwser:
 
