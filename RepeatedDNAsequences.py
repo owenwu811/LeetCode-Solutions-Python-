61eb4edecd755e75aa88fc324f63a5ebdbb0c0ce
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-10-28 17:38:30 -0400

    Update RepeatedDNAsequences.py

--- RepeatedDNAsequences.py
+++ RepeatedDNAsequences.py
@@ -39,9 +39,14 @@ Constraints:
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         seen, res = set(), set()
-        for l in range(len(s) - 9):
-            cur = s[l:l + 10]
+        for i in range(len(s) - 9): #len = 13, so index 3 is the last inclusive before out of bounds - 0, 1, 2, 3 
+            cur = s[i: i + 10] #3:13 means index 12 is the last inclusive
             if cur in seen:
                 res.add(cur)
             seen.add(cur)
-        return list(res)
+        return res
+        
+        #PYTHON SETS ORDER DOES NOT MATTER
+        #s = "AAAAAAAAAAAAA" - len of 13 index 3 last inclusive windowstart
+        #3:13 - index 12 last inclusive meaning last windowend that's valid 
+        # since sets don't count order, as long as we have already seen this 10 character pair, we can add it to the result because we are looking for 10 letter long sequences that occur more than ONCE. seen.add(cur) adds every 10 character pair we ever see, and the pair is denoted by s[i:i + 10] instead of windowstart and windowend
