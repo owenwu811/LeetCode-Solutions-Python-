df421ca3a6e90c37b3398e57d0eaff2ca1d9b61c
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-11-10 22:18:03 -0500

    Update NumberofIslands.py

--- NumberofIslands.py
+++ NumberofIslands.py
@@ -10,19 +10,19 @@
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(down, right):
+        def dfs(down, right, grid):
             if down < 0 or down >= len(grid) or right < 0 or right >= len(grid[0]) or grid[down][right] == '0':
-                return
-            grid[down][right] = '0'
-            dfs(down + 1, right)
-            dfs(down - 1, right)
-            dfs(down, right + 1)
-            dfs(down, right - 1)
-
-        count = 0
-        for down in range(len(grid)):
-            for right in range(len(grid[0])):
-                if grid[down][right] == '1':
+                return #out of bounds, so return out of the function
+            grid[down][right] = '0' #sinking the 1 into a 0 so we don't double count it
+            dfs(down + 1, right, grid)
+            dfs(down - 1, right, grid)
+            dfs(down, right + 1, grid)
+            dfs(down, right - 1, grid)
+        count = 0 #we will return this at the end
+        for down in range(len(grid)): #right
+            for right in range(len(grid[0])): #down
+                if grid[down][right] == '1': #we found a 1, so we found atleast 1 island
                     count += 1
-                    dfs(down, right)
-        return count
+                    dfs(down, right, grid) #we must do a boundary check before sinking that 1 into a 0!!!!!!
+        return count 
+        
