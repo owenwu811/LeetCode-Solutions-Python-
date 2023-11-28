cebfab76bcc38c09043d570947373c8b463e8e40
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-11-27 19:59:54 -0500

    Create countwordswithagivenprefix.py

---
+++ countwordswithagivenprefix.py
@@ -0,0 +1,25 @@
+#You are given an array of strings words and a string pref.
+
+#Return the number of strings in words that contain pref as a prefix.
+
+#A prefix of a string s is any leading contiguous substring of s.
+
+ 
+
+#Example 1:
+
+#Input: words = ["pay","attention","practice","attend"], pref = "at"
+#Output: 2
+#Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+
+#my python3 solution:
+
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        res = 0
+        lastpref = len(pref) - 1
+        for word in words:             
+            if pref in word and word[0] == pref[0] and word[lastpref] == pref[-1]:
+                res += 1                
+        return res
+                
