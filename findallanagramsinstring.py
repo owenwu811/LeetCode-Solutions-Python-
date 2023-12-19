48de53e430d4f576c16fa548fe0ab26dcd6a88e5
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-12-19 17:44:43 -0500

    Update findallanagramsinstring.py

--- findallanagramsinstring.py
+++ findallanagramsinstring.py
@@ -29,4 +29,17 @@ class Solution:
             if window == key:
                 res.append(char)
         return res
+
+
+#my solution - python3
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        res = []
+        key = "".join(sorted(p))
+        for char in range(len(s) - len(p) + 1):
+            k = "".join(sorted(s[char: char + len(p)]))
+            if k == key:
+                res.append(char)
+        return res
         
