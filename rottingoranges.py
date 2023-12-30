7241b160b9b03f0d458dbdab013d5b3511358afe
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-12-30 11:34:51 -0500

    Update rottingoranges.py

--- rottingoranges.py
+++ rottingoranges.py
@@ -154,3 +154,48 @@ class Solution:
                     rottingorangesl.append([destroyx, destroyy])
             minminutes += 1
         return minminutes if numberoffresh == 0 else -1
+
+
+#12/20/23 refresher with explanation python3 - my solution: 
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        #each deque will represent original rotten oranges from a particular timeslot 
+        d = deque()
+        minminutes = 0
+        numberoffresh = 0
+        #find all rotten oranges from:
+        for row in range(len(grid)):
+            for column in range(len(grid[0])):
+                if grid[row][column] == 2:
+                    #indicies of each rotten orange we found at time 0
+                    d.append([row, column])
+                elif grid[row][column] == 1:
+                    numberoffresh += 1
+                else:
+                    continue
+        #since we found all rotten oranges from a particular timeslot, we now want to simulate the spread of rotting and correlate the spread to each minute progression
+        #remember, we can spread the disease in all 4 directions aka 4 directionally adjacent
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        #since each deque represents the coordinates of each rotten orange from a particular timeslot, we want to start the spread, so we have to loop through the deque, a list of lists, and since the values of the list is the x and y of the rotten orange from the original timeslot, we want to add this original x and y value to all 4 x and y values from each sublist in directions
+        #remember, this entire block of code below this represents the simulation process of spreading disease at a particular timeslot, which is why we pop from the deque after counting that organge from a particular timeslot and then add to the deque the new destroyed orange since the new destroyed orange can spread to other destroyed oranges at a new timeslot while the original bad orange can only destroy adjacently once in all directions from it's own timeslot
+        while d and numberoffresh > 0:
+            #since each deque represents each timeslot, the change in time should line up with this
+            for index in range(len(d)):
+                #we have to get rid of each coordinate from the deque as we can't count the same orange twice from the same timeslot 
+                xoforiginal, yoforiginal = d.popleft()
+                for xofnew, yofnew in directions:
+                    destroyx, destroyy = xoforiginal + xofnew, yoforiginal + yofnew
+                    #while stepping onto this new coordinate that we will spread the disease to, this new coordinate must be in the grid and also must be a fresh orange to rot, not an already rotten orange (2) or an empty cell (0)
+                    #if this new grid we are stepping onto in one particular sublist direction from the directions sublist + our original coordinate is out of bounds or not a fresh orange, we can continue since this is not recursive but iterative
+                    if destroyx < 0 or destroyx >= len(grid) or destroyy < 0 or destroyy >= len(grid[0]) or grid[destroyx][destroyy] != 1:
+                        continue
+                    #if this new grid from a particular timeslot and a particular direction is a fresh orange and in the grid, then we can make that fresh orange rotten by turning the 1 in that new grid to a 2
+                    grid[destroyx][destroyy] = 2
+                    #since this new grid from a particular timeslot and direction's 1 became a 2, we need to reflect that one less fresh orange in our count of fresh oranges to determine if we can even rot all FRESH oranges we have 
+                    numberoffresh -= 1
+                    #since this new grid from a particular timeslot and direction's 1 became a 2, we now have a new rotten orange, that, from the NEXT TIMESLOT, can spread to other fresh oranges in all 4 directions, so add this slot's coordinates to the deque
+                    d.append([destroyx, destroyy])
+            minminutes += 1
+        #after we have no more rotten oranges to rot or no more fresh oranges, we return minminutes. but if we still have fresh oranges that have not ever been adjacent to a rotting orange after this entire process of spreading at each timeslot and stepping onto new directions in the grid, we return -1 - this is the case where deque is empty meaning no more diseases to spread or rotten oranges to spread but still have fresh oranges
+        return minminutes if numberoffresh == 0 else -1
