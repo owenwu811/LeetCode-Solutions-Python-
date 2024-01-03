bf7d476923a4a0de5858941af6911742ee9132ac
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-03 18:28:41 -0500

    Update combinationsum.py

--- combinationsum.py
+++ combinationsum.py
@@ -78,3 +78,22 @@ class Solution:
             f(index + 1, ans, windowsum)
         f(0, [], 0)
         return res
+
+#1/3/24 refresher:
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def f(index, ans, windowsum):
+            if windowsum == target:
+                res.append(ans.copy())
+                return
+            if index >= len(candidates) or windowsum > target:
+                return
+            ans.append(candidates[index])
+            f(index, ans, windowsum + candidates[index])
+            ans.pop()
+            f(index + 1, ans, windowsum)
+
+        res = []
+        f(0, [], 0)
+        return res
