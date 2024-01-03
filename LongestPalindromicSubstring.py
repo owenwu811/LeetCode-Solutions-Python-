590b300f5036322460c6e5a060cf8b88ebeaba2a
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-03 18:47:22 -0500

    Update and rename LongestPalindromicSubstring to LongestPalindromicSubstring.py

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -1,34 +1,34 @@
-5. Longest Palindromic Substring
-Medium
-25.8K
-1.5K
-Companies
-Given a string s, return the longest 
-palindromic
+#5. Longest Palindromic Substring
+#Medium
+#25.8K
+#1.5K
+#Companies
+#Given a string s, return the longest 
+#palindromic
  
-substring
- in s.
+#substring
+# in s.
 
- 
-
-Example 1:
-
-Input: s = "babad"
-Output: "bab"
-Explanation: "aba" is also a valid answer.
-Example 2:
-
-Input: s = "cbbd"
-Output: "bb"
  
 
-Constraints:
+#Example 1:
 
-1 <= s.length <= 1000
-s consist of only digits and English letters.
+#Input: s = "babad"
+#Output: "bab"
+#Explanation: "aba" is also a valid answer.
+#Example 2:
+
+#Input: s = "cbbd"
+#Output: "bb"
+ 
+
+#Constraints:
+
+#1 <= s.length <= 1000
+#s consist of only digits and English letters.
 
 
-My Solution (Python):
+#My Solution (Python):
 
 #We are expanding outwards from the center by comparing left and right values. If left and right values match, we keep expanding outwards until characters either 1. no longer match or 2. reach the boundaries of the string
 
@@ -49,3 +49,27 @@ class Solution(object):
                 result = test
         
         return result #at the end, result will contain the longest palindromic substring found in the given string s
+
+
+
+#1/3/24 refresher:
+
+class Solution:
+    def longestPalindrome(self, s):
+        def f(l, r, s):
+            #making sure we are inside the string with the two pointers and that the characters from both pointers are the same. If so, we expand outward forever until one of these conditions is broken to find an even longer palindromic substring. 
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            #if the while loop is broken meaning we are outside the string or one side is nor equal to the other side character, then we need to step backwards and include the substring that was valid by moving left forward one and right backward one from the point left and right were at when this while loop was violated and return that to be compared
+            return s[l + 1: r]
+        res = ""
+        for char in range(len(s)):
+            first = f(char, char, s)
+            if len(first) > len(res):
+                res = first
+            second = f(char, char + 1, s)
+            if len(second) > len(res):
+                res = second
+        return res
+        
