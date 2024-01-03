a304eb0ba0584ce0d4516e989e0724d1e650f4dc
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-03 18:34:03 -0500

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -118,3 +118,22 @@ class Solution:
 #f(index + 1): Recursively call the function with the next index to exclude the current element from the subset.
 
 #If you were to replace f(index + 1) with f(index) in the code, it would result in an infinite loop. You would keep calling the same index element over and over again without ever hitting the base case of if index >= len(nums) to stop recursion and return a value (nothing in this case) back to the parent function
+
+
+#1/3/24 refresher - my solution python3:
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def f(index, ans):
+            if index >= len(nums):
+                res.append(ans.copy())
+                return
+            ans.append(nums[index])
+            f(index + 1, ans)
+            ans.pop()
+            f(index + 1, ans)
+
+        res = []
+        f(0, [])
+        return res
