ee5c203312b5735fcd697461d99b8bb190c203aa
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-06 10:24:45 -0500

    Update FloodFill.py

--- FloodFill.py
+++ FloodFill.py
@@ -176,3 +176,21 @@ class Solution:
         self.fill(image, sr - 1, sc, color, starting)
         self.fill(image, sr, sc + 1, color, starting)
         self.fill(image, sr, sc - 1, color, starting)
+
+#1/6/24 refresher solution:
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        starting = image[sr][sc]
+        if starting == color:
+            return image
+        self.fill(starting, image, sr, sc, color)
+        return image
+    def fill(self, starting, image, sr, sc, color):
+        if sr < 0 or sr >= len(image) or sc < 0 or sc >= len(image[-1]) or image[sr][sc] != starting:
+            return
+        image[sr][sc] = color
+        self.fill(starting, image, sr + 1, sc, color)
+        self.fill(starting, image, sr - 1, sc, color)
+        self.fill(starting, image, sr, sc + 1, color)
+        self.fill(starting, image, sr, sc - 1, color)
