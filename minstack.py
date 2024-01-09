f656ba67fc9cac89e728715c87fe1d3fd4cb6a99
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-09 18:29:23 -0500

    Update minstack.py

--- minstack.py
+++ minstack.py
@@ -78,4 +78,32 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.minstack[-1]
+
+
+
+
+#1/9/24 refresher:
+
+class MinStack:
+    def __init__(self):
+       self.stack = []
+       self.minstack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minstack:
+            val = min(val, self.minstack[-1])
+        else:
+            val
+        self.minstack.append(val)
+        
+    def pop(self) -> None:
+        self.stack.pop()
+        #we also need to pop the element off minstack because we want to return the smallest element in the last function as well!
+        self.minstack.pop()
       
+    def top(self) -> int:
+        return self.stack[-1]
+      
+    def getMin(self) -> int:
+        return self.minstack[-1]
