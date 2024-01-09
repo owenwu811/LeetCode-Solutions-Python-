f65d649acaeecff2680ea4ba03752651f19d7979
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-09 11:38:40 -0500

    Create uniquepaths.py

---
+++ uniquepaths.py
@@ -0,0 +1,26 @@
+#There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+#Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+#The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+
+
+
+#python3 solution:
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        #think about this like pascal's triangle tilted
+        row = [1] * n
+        #number of iterations for rows 
+        for r in range(m - 1):
+            #modified row
+            newrow = [1] * n
+            #number of iterations for columns - (n - 1)
+            for column in range(n - 2, -1, -1):
+                #pascal's triangle rule - build off of the first array called row
+                newrow[column] = newrow[column + 1] + row[column]
+            row = newrow
+        return row[0]
+
