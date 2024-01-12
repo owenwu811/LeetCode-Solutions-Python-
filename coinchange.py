f1b9c1fb6a9f3359b08f27d924f1367037a657aa
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-12 18:04:45 -0500

    Update coinchange.py

--- coinchange.py
+++ coinchange.py
@@ -7,7 +7,19 @@
 
 
 
+#You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
 
+#Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+#You may assume that you have an infinite number of each kind of coin.
+
+ 
+
+#Example 1:
+
+#Input: coins = [1,2,5], amount = 11
+#Output: 3
+#Explanation: 11 = 5 + 5 + 1
 
 
 #python3 solution:
@@ -27,4 +39,13 @@ class Solution:
             return -1
         else:
             return dparr[-1]
+
+
+
+#note that 
+
+dparr = [0] + ([float('inf')] * 7)
+print(dparr)
+
+#would output [0, inf] as infinity times anything equals infinity
         
