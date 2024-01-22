0afffd871e4b82d87776d5ba3762b55b96becf1b
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-21 22:10:16 -0500

    Update NumberofIslands.py

--- NumberofIslands.py
+++ NumberofIslands.py
@@ -183,3 +183,26 @@ class Solution:
                     res += 1
                     dfs(grid, r, c)
         return res
+
+
+#1/21/24 refresher:
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(grid, r, c):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == "0":
+                return
+            #prevent infinite recursion while looking for parts of the same island
+            grid[r][c] = "0"
+            dfs(grid, r + 1, c)
+            dfs(grid, r - 1, c)
+            dfs(grid, r, c + 1)
+            dfs(grid, r, c - 1)
+
+        res = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1":
+                    res += 1
+                    dfs(grid, r, c)
+        return res
