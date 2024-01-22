7bfa4db6dfc5c5d058ab33f80fb833c43cd2ae22
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-22 10:09:17 -0500

    Update subsets.py

--- subsets.py
+++ subsets.py
@@ -213,3 +213,22 @@ class Solution:
         res = []
         f(0, nums, [])
         return res
+
+
+#1/22/24 refresher:
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def dfs(i, path):
+            if i >= len(nums):
+                res.append(path.copy())
+                return
+            path.append(nums[i])
+            dfs(i + 1, path)
+            path.pop()
+            dfs(i + 1, path)
+
+        res = []
+        dfs(0, [])
+        return res
