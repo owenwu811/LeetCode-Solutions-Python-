42d406a94f74800f3542c5c1b967ebf8f1a37f33
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-24 15:58:58 -0500

    Update ValidPalindrome.py

--- ValidPalindrome.py
+++ ValidPalindrome.py
@@ -95,3 +95,19 @@ while l < r:
     l += 1
     r -= 1
 return True
+
+
+#1/24/24 refresher:
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        s.replace(" ", "")
+        s = re.sub("[\W_]", "", s)
+        l, r = 0, len(s) - 1
+        while l < r:
+            if s[l].casefold() != s[r].casefold():
+                return False
+            l += 1
+            r -= 1
+        return True
