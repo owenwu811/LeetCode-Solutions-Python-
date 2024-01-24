f7b23dcdc16d15b80243dd02ba635e4e61f4b13a
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-24 13:07:24 -0500

    Update NumberofIslands.py

--- NumberofIslands.py
+++ NumberofIslands.py
@@ -206,3 +206,27 @@ class Solution:
                     res += 1
                     dfs(grid, r, c)
         return res
+
+
+#1/24/24 refresher:
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(grid, r, c): 
+            #if we are out of bounds or see water, which won't be true the 1st time this conditional is evaluated, we return nothing back to the parent caller
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == "0":
+                return
+            #set it equal to 0 so we don't double count it and run into infinite recursion
+            grid[r][c] = "0"
+            dfs(grid, r + 1, c)
+            dfs(grid, r - 1, c)
+            dfs(grid, r, c + 1)
+            dfs(grid, r, c - 1)
+
+        count = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1":
+                    count += 1
+                    dfs(grid, r, c)
+        return count
