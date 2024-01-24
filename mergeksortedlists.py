df4be3af3c303934961d4b76fb5233f91f46894b
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-24 13:25:34 -0500

    Update mergeksortedlists.py

--- mergeksortedlists.py
+++ mergeksortedlists.py
@@ -227,5 +227,46 @@ class Solution:
             return dummy.next
                     
 
+
+#1/24/24 refresher:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+        def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+            if not lists:
+                return None
+            while len(lists) > 1:
+                #we don't want duplicate nodes 
+                mergedlists = []
+                for i in range(0, len(lists), 2):
+                    list1 = lists[i]
+                    if (i + 1) < len(lists):
+                        list2 = lists[i + 1]
+                    else:
+                        list2 = None
+                    mergedlists.append(self.ml(list1, list2))
+                lists = mergedlists
+            return lists[0]
+        def ml(self, list1, list2):
+            dummy = ListNode(0)
+            current = dummy
+            while list1 and list2:
+                if list1.val < list2.val:
+                    current.next = list1
+                    list1 = list1.next
+                else:
+                    current.next = list2
+                    list2 = list2.next
+                current = current.next
+            if list1 and not list2:
+                current.next = list1
+            elif list2 and not list1:
+                current.next = list2
+            return dummy.next
             
 
