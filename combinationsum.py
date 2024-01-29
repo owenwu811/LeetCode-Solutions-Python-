c3e52e3a7a86f6c983ace6635b50218e0d48f564
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-29 10:13:14 -0500

    Update combinationsum.py

--- combinationsum.py
+++ combinationsum.py
@@ -208,4 +208,22 @@ class Solution:
         dfs(0, [], 0)
         return res
         
-        
+
+
+#1/29/24 refresher:
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def dfs(index, ans, windowsum):
+            if windowsum == target:
+                res.append(ans.copy())
+                return
+            elif index >= len(candidates) or windowsum > target:
+                return
+            ans.append(candidates[index])
+            dfs(index, ans, windowsum + candidates[index])
+            ans.pop()
+            dfs(index + 1, ans, windowsum)
+        res = []
+        dfs(0, [], 0)
+        return res
