e7e413719ec04ac063c1b18ffe03e93a0442dcd3
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-31 17:44:51 -0500

    Update validatebinarysearchtree.py

--- validatebinarysearchtree.py
+++ validatebinarysearchtree.py
@@ -236,3 +236,25 @@ class Solution:
                 return False
             return (dfs(root.left, lowerbound, root.val) and dfs(root.right, root.val, upperbound))
         return dfs(root, float('-inf'), float('inf'))
+
+
+#1/31/24 refresher:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def valid(root, lowerbound, upperbound):
+            if not root:
+                return True
+            if not (lowerbound < root.val < upperbound):
+                return False
+            #we will check every level of the left and right subtrees and return a boolean for each level - if False, we can already return False as the entire result, which will trigger the and to return False since it's lazy, which will propogate to the original recursive call
+            return (valid(root.left, lowerbound, root.val) and valid(root.right, root.val, upperbound))
+        
+        return valid(root, float('-inf'), float('inf'))
+            
