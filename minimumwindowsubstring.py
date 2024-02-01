c0e172faaf4a07fb32ff6e9dc023e0e666c212b5
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-01-31 20:10:35 -0500

    Update minimumwindowsubstring.py

--- minimumwindowsubstring.py
+++ minimumwindowsubstring.py
@@ -30,3 +30,40 @@ class Solution:
 
 #explanation: ORIGINALLY, our tdict represents what we need from s to satisfy t: 'a': 1, 'b': 1, 'c': 1. as we find t's characters from s, we now say we need ONE LESS FROM S. if we shrink the window, we take the character - a - we kicked out of the left side of the window and saw we now still need one MORE OF THAT CHARACTER FROM S by incrementing the dictionary value - a: 0 becomes a: 1 - to say we still need one more A from s to satisfy t. 
 #explanation 2: ONLY WHAT IS IN OUR WINDOW CURRENTLY can count towards our allowance from s to satisfy t. We only shrink the window once by kicking the character out from the leftmost side of our window, and if the character - a - we kicked out counted towards the allowance towards satisfying t, we must take this allowance away once since we only shrink by a character ONCE and increment the count in the dictinoary to say we still need one more a - 'a': 0 becomes 'a': 1 to say we still need one more a to satisfy t
+
+
+
+#my solution - python3:
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t): return ""
+        startsaver = 0
+        endsaver = len(s) + 1
+        ws = 0
+        matched = 0
+        tdict = Counter(t)
+        for we, schar in enumerate(s):
+            if schar in tdict:
+                tdict[schar] -= 1
+                #only matched when we satisfy all occurences of a character 
+                if tdict[schar] == 0:
+                    matched += 1
+            while matched == len(tdict):
+                if endsaver > we - ws + 1:
+                    startsaver = ws
+                    endsaver = we - ws + 1
+                kickout = s[ws]
+                ws += 1
+                if kickout in tdict:
+                    #only if we completely satisfied the character in t do we rollback our progress on matched  
+                    if tdict[kickout] == 0:
+                        matched -= 1
+                    #we need one more
+                    tdict[kickout] += 1
+        if len(s) > endsaver:
+            return s[startsaver:startsaver + endsaver]
+        elif len(s) == endsaver:
+            return s
+        else:
+            return ""
