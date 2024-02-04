096693a80845d6853edba71a45355d6d71470f3b
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-02-03 19:32:14 -0500

    Update implementqueueusingstacks.py

--- implementqueueusingstacks.py
+++ implementqueueusingstacks.py
@@ -154,3 +154,33 @@ class MyQueue:
     def empty(self) -> bool:
         return len(self.d) == 0
       
+
+
+#2/3/24 refresher:
+
+class MyQueue:
+
+    def __init__(self):
+        self.stack = []
+        
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+  
+  
+     
+ 
+    def pop(self) -> int:
+        return self.stack.pop(0)
+       
+    
+
+    def peek(self) -> int:
+        return self.stack[0]
+ 
+        
+    
+
+    def empty(self) -> bool:
+        return len(self.stack) == 0
+      
