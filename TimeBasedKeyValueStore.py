3ca4f443d80b76a67ed7873727ffb18630b2fd92
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-02-10 20:56:49 -0500

    Update TimeBasedKeyValueStore.py

--- TimeBasedKeyValueStore.py
+++ TimeBasedKeyValueStore.py
@@ -141,3 +141,31 @@ class TimeMap:
                 r = mid - 1
         return res
      
+
+#2/10/24 practice again:
+
+class TimeMap:
+
+    def __init__(self):
+        self.cachd = dict()
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.cachd:
+            self.cachd[key] = []
+        self.cachd[key].append([value, timestamp])
+        
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        #gives us [value, timestamp]
+        c = self.cachd.get(key, [])
+        l, r = 0, len(c) - 1
+        while l <= r:
+            #since we know timestamps were already inserted in order, we can just search for the one we want and try to get the closest to it that is less than it
+            mid = (l + r) // 2
+            if c[mid][1] <= timestamp:
+                res = c[mid][0]
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
