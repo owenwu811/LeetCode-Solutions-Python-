7db6a66ed006835a9c972dfce4ab165efe36c9c7
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-02-11 13:55:45 -0500

    Update TimeBasedKeyValueStore.py

--- TimeBasedKeyValueStore.py
+++ TimeBasedKeyValueStore.py
@@ -169,3 +169,29 @@ class TimeMap:
             else:
                 r = mid - 1
         return res
+
+#2/11/24:
+
+class TimeMap:
+
+    def __init__(self):
+        self.cachd = dict()
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.cachd:
+            self.cachd[key] = []
+        self.cachd[key].append([value, timestamp])
+        
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        pair = self.cachd.get(key, [])
+        l, r = 0, len(pair) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if pair[mid][1] <= timestamp:
+                res = pair[mid][0]
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
