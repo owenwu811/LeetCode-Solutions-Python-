a1429738c8e2778fae7134f1d6e9aa32e7525eac
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-02-13 10:52:09 -0500

    Update TimeBasedKeyValueStore.py

--- TimeBasedKeyValueStore.py
+++ TimeBasedKeyValueStore.py
@@ -231,3 +231,34 @@ class TimeMap:
             else:
                 r = mid - 1
         return res
+
+#2/13/24:
+
+class TimeMap:
+
+    def __init__(self):
+        self.cachd = dict()
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.cachd:
+            #we know that the [value, timestamp] are correlated as a list, so do add an empty list even if the key dosen't exist in the dict yet
+            self.cachd[key] = []
+        #the timestamps are being added in order already, so we can just run a binary search without sorting again
+        self.cachd[key].append([value, timestamp])
+       
+        
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        #we want the largest but closest to without exceeding timestamp in our list as the timestamp given in the request
+        correspondence = self.cachd.get(key, [])
+        l, r = 0, len(correspondence) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            #closest to timestamp_prev means can be equal to
+            if correspondence[mid][1] <= timestamp:
+                res = correspondence[mid][0]
+                l = mid + 1
+            else: # we can't exceed the request
+                r = mid - 1
+        return res
