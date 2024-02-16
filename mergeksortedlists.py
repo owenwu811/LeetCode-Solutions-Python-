32d7e75e6798e7280940a6c4f98dbd323820f28a
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-02-16 17:08:12 -0500

    Update mergeksortedlists.py

--- mergeksortedlists.py
+++ mergeksortedlists.py
@@ -470,3 +470,43 @@ class Solution:
         elif list2 and not list1:
             current.next = list2
         return prev.next
+
+#2/16/24:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None #head of nothing
+        while len(lists) > 1:
+            merged = []
+            for i in range(0, len(lists), 2):
+                list1 = lists[i]
+                if (i + 1) < len(lists):
+                    list2 = lists[i + 1]
+                else:
+                    list2 = None
+                merged.append(self.find(list1, list2))
+            lists = merged
+        return lists[0]
+    def find (self, list1, list2):
+        previous = ListNode(None)
+        current = previous
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        if list1 and not list2:
+            current.next = list1
+        else:
+            current.next = list2
+        return previous.next
+                    
