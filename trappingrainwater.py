2986d137022b23292710f7ec18ee458444d56072
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-02-17 11:42:33 -0500

    Update trappingrainwater.py

--- trappingrainwater.py
+++ trappingrainwater.py
@@ -189,3 +189,23 @@ class Solution:
                 maxleft = max(maxleft, height[l])
                 result += (maxleft - height[l])
         return result
+
+#2/17/24:
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        l, r = 0, len(height) - 1
+        res = 0
+        maxleft, maxright = height[l], height[r]
+        while l < r:
+            if maxleft < maxright:
+                l += 1
+                maxleft = max(maxleft, height[l])
+                res += (maxleft - height[l])
+            else:
+                r -= 1
+                maxright = max(maxright, height[r])
+                res += (maxright - height[r])
+        return res
