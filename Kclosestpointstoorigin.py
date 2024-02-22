9bc1420184bbd7346c42f33b6fda363803d834d1
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-02-22 10:07:36 -0500

    Update Kclosestpointstoorigin.py

--- Kclosestpointstoorigin.py
+++ Kclosestpointstoorigin.py
@@ -116,3 +116,19 @@ class Solution:
             result.append([x, y])
             k -= 1
         return result
+
+#2/22/24:
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minheap = []
+        for x, y in points:
+            distance = (x ** 2) + (y ** 2)
+            minheap.append([distance, x, y])
+        heapq.heapify(minheap) 
+        result = []
+        while k > 0:
+            distance, x, y = heapq.heappop(minheap)
+            result.append([x, y])
+            k -= 1
+        return result
