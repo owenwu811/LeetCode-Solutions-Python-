15206759726d750914d42ee75a4e82699b93558d
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-02-23 11:06:47 -0500

    Update FirstBadVersion.py

--- FirstBadVersion.py
+++ FirstBadVersion.py
@@ -159,3 +159,22 @@ class Solution:
             else:
                 l = mid + 1
         return l
+
+
+#2/23/24:
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l = 0
+        r = n
+        while l <= r:
+            mid = (l + r) // 2
+            if isBadVersion(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
+            
