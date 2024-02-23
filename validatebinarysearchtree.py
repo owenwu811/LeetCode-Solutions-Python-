19cf9ee7aed3197c87e84c77fe57dfb2b2b8d14f
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-02-23 15:17:16 -0500

    Update validatebinarysearchtree.py

--- validatebinarysearchtree.py
+++ validatebinarysearchtree.py
@@ -317,3 +317,22 @@ class Solution:
             #lazy and - right subtree will only evaluate if and only if left subtree returns true for all of it's layers
             return f(root.left, lowerbound, root.val) and f(root.right, root.val, upperbound)
         return f(root, float('-inf'), float('inf'))
+
+
+#2/23/24:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def f(root, lowerbound, upperbound):
+            if not root:
+                return True
+            if not (lowerbound < root.val < upperbound):
+                return False
+            return f(root.left, lowerbound, root.val) and f(root.right, root.val, upperbound)
+        return f(root, float('-inf'), float('inf'))
