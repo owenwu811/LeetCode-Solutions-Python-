029d2abd5fe3b42213c01a2aab43298f6c792ac3
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-02-25 16:05:03 -0500

    Update LowestCommonAncestorofaBinaryTree.py

--- LowestCommonAncestorofaBinaryTree.py
+++ LowestCommonAncestorofaBinaryTree.py
@@ -46,3 +46,23 @@ class Solution:
         elif l is not None and r == None: return l #because l equals to p or q
         elif r is not None and l == None: return r
         else: return None
+
+#2/25/24:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == None or root == p or root == q:
+            return root
+        l = self.lowestCommonAncestor(root.left, p, q)
+        r = self.lowestCommonAncestor(root.right, p, q)
+        if l and r: return root
+        elif l == None and r != None: return r
+        elif r == None and l != None: return l
+        else: return None
