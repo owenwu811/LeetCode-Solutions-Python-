a5226f68b905d11c3d63f4a778bb33ed52b6c635
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-08 21:15:38 -0500

    Update TimeBasedKeyValueStore.py

--- TimeBasedKeyValueStore.py
+++ TimeBasedKeyValueStore.py
@@ -368,3 +368,28 @@ class TimeMap:
             else:
                 r = mid - 1
         return res
+
+
+#3/8/24:
+
+class TimeMap:
+    def __init__(self):
+        self.mydict = dict()
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.mydict:
+            self.mydict[key] = []
+        self.mydict[key].append([value, timestamp])
+       
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        c = self.mydict.get(key, []) #corresponds to a [value, timestamp]
+        l, r = 0, len(c) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if c[mid][1] <= timestamp:
+                res = c[mid][0]
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
