ceb397a96cd61f02fc6c05818b4f29e58b804101
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-10 14:30:34 -0400

    Create ImplementTrie.py

---
+++ ImplementTrie.py
@@ -0,0 +1,41 @@
+
+#A trie (pronounced as "try") or prefix tree is a tree data structure used to efficiently store and retrieve keys in a dataset of strings. There are various applications of this data structure, such as autocomplete and spellchecker.
+
+#Implement the Trie class:
+
+#Trie() Initializes the trie object.
+#void insert(String word) Inserts the string word into the trie.
+#boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
+#boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
+
+#Input
+#["Trie", "insert", "search", "search", "startsWith", "insert", "search"]
+#[[], ["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+#Output
+#[null, null, true, false, true, null, true]
+
+
+
+#my solution - python3:
+
+class Trie:
+
+    def __init__(self):
+        self.mydict = dict()
+        
+
+    def insert(self, word: str) -> None:
+        self.mydict[word] = []
+        
+
+    def search(self, word: str) -> bool:
+        if word not in self.mydict:
+            return False
+        return True
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        for d in self.mydict:
+            if d.startswith(prefix): #d.startswith is needed!
+                return True
+        return False
