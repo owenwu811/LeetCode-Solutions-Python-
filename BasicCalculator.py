59e2dc95585434df0e59b0431b53818bc738ee2a
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-11 14:25:28 -0400

    Update BasicCalculator.py

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -223,3 +223,29 @@ class Solution:
                 currentn = 0
         ressum += (currentn * sign) #can do this as well 
         return ressum
+
+
+#3/11/24:
+
+class Solution:
+    def calculate(self, s: str) -> int:
+        ressum, sign, currentn, stack = 0, 1, 0, []
+        for char in s:
+            if char.isdigit():
+                currentn = currentn * 10 + int(char)
+            elif char in '+-':
+                ressum += currentn * sign
+                sign = -1 if char == "-" else 1
+                currentn = 0
+            elif char == "(":
+                stack.append(ressum)
+                stack.append(sign)
+                ressum, sign = 0, 1
+            elif char == ")":
+                ressum += currentn * sign
+                ressum *= stack.pop()
+                ressum += stack.pop()
+                currentn = 0
+        ressum += currentn * sign
+        return ressum
+        
