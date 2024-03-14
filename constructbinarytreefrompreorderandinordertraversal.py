0b929b1abac9ad7d986175fef5559768f3b661d5
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-13 20:20:26 -0400

    Update constructbinarytreefrompreorderandinordertraversal.py

--- constructbinarytreefrompreorderandinordertraversal.py
+++ constructbinarytreefrompreorderandinordertraversal.py
@@ -30,7 +30,7 @@ class Solution:
         #axe 3
 
         #p = [ 3(axed), 9, 20, 15, 7] - this corresponds to preorder[1:mid + 1] - now, 9 becomes the root of the tree instead of 3! - 1 value in left + 3 values in right (partition)
-        #i = [9, 3(axed), 15, 20, 7] - this corresponds to inorder[:mid]
+        #i = [9, 3(axed), 15, 20, 7] - this corresponds to inorder[:mid] - so since left is 1 node and right is 3 nodes (for i), they tell us how to partition the p array! so we cut right after 9 in p = [3, 9, 20, 15, 7], so [9] by itself is size 1, and [20, 15, 7] is size 3, and then from here, with the [9] by itself, we will recursively run the algorithm to create the left subtree, and with [20, 15, 7], we will recursively run the algorithm to create the right subtree from our input, and we repeat this process until we get to our base cases and we finish with every node we need to create
 
        #axe 9
 
