75e4027d1706ef6fdae388581a9842f82ed09abb
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-13 20:03:39 -0400

    Update constructbinarytreefrompreorderandinordertraversal.py

--- constructbinarytreefrompreorderandinordertraversal.py
+++ constructbinarytreefrompreorderandinordertraversal.py
@@ -16,12 +16,12 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder:
+        if not preorder or not inorder: #base case 
             return None
         root = TreeNode(preorder[0]) #root of tree
         mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid]) #look at ordering of method
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:]) 
         return root
 
         #p = [3, 9, 20, 15, 7]
