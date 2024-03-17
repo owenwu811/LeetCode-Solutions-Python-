5fb7e080ff206900d6ec8b15d86456c7b8efc32c
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-17 11:57:19 -0400

    Update clonegraph.py

--- clonegraph.py
+++ clonegraph.py
@@ -202,3 +202,30 @@ class Solution:
                 copy.neighbors.append(dfs(n))
             return copy
         return dfs(node) if node else None
+
+
+#3/17/24:
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None): 
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        self.mydict = {} #{original:cloned}
+        def f(node): #clone nodes recursively 
+            if node in self.mydict:
+                return self.mydict[node] #already cloned
+            copy = Node(node.val) #read the class definition - there is no TreeNode class, just a Node class! Has nothing to do with using self or not - both are fine here
+            self.mydict[node] = copy #creating a new record in the dictionary
+            for original in node.neighbors:
+                copy.neighbors.append(f(original)) #now copy does have neighbors attribute because of the class definition above. this part creates the edges between the cloned graph
+            return copy #return the copy
+        return f(node) if node else None #no graph to clone if original node == None
+         
