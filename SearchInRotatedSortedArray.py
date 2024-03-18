06ef9632dad6fbaa439fba1396b1c8192e61f86e
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-17 20:26:04 -0400

    Update SearchInRotatedSortedArray.py

--- SearchInRotatedSortedArray.py
+++ SearchInRotatedSortedArray.py
@@ -79,3 +79,12 @@ class Solution:
             if j == target:
                 return i
         return -1
+
+#3/17/24:
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        for x, y in enumerate(nums):
+            if y == target:
+                return x
+        return -1
