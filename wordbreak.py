b325b975d26877adf110790f6f35df32bd5044f6
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-18 21:14:40 -0400

    Update wordbreak.py

--- wordbreak.py
+++ wordbreak.py
@@ -206,3 +206,15 @@ class Solution:
                 if s[i:j + 1] in wordDict:
                     res[j + 1] = res[i] or res[j + 1]
         return res[-1]
+
+#3/18/24:
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        #the same word in the dictionary can be reused more than once to satisfy s
+        res = [True] + ([False] * len(s))
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if s[i:j + 1] in wordDict:
+                    res[j + 1] = res[i] or res[j + 1]
+        return res[-1]
