18f29ccd91430b11c89da9fac2e24291e8d5ff9a
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-20 12:12:09 -0400

    Update minstack.py

--- minstack.py
+++ minstack.py
@@ -494,3 +494,38 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.minstack[-1]
+
+#3/20/24:
+
+class MinStack:
+
+    def __init__(self):
+        self.stack, self.minstack = [], []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minstack:
+            val = min(val, self.minstack[-1])
+        else:
+            val
+        self.minstack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minstack.pop()
+        
+        
+      
+    
+    def top(self) -> int:
+        return self.stack[-1]
+      
+     
+
+  
+        
+
+    def getMin(self) -> int:
+        return self.minstack[-1]
+
