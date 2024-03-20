b5a5b165070f2f8ccf6e80c72664de4676dbc1ea
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-19 22:34:41 -0400

    Update lettercombinationsofaphonenumber.py

--- lettercombinationsofaphonenumber.py
+++ lettercombinationsofaphonenumber.py
@@ -281,3 +281,22 @@ class Solution:
         res = []
         f("")
         return res
+
+
+#3/19/24:
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+        mapping = {"2": ['a', 'b', 'c'], "3": ['d', 'e', 'f'], "4": ['g', 'h', 'i'], "5": ['j', 'k', 'l'], "6": ['m', 'n', 'o'], "7": ['p', 'q', 'r', 's'], "8": ['t', 'u', 'v'], "9": ['w', 'x', 'y', 'z']}
+        def f(path):
+            if len(path) == len(digits):
+                res.append(path) #again, string has no .copy attribute, and you don't need it either because the path that was appended won't be modified because path and i reset I think after the base case hits?
+                return
+            else:
+                for i in mapping[digits[len(path)]]:
+                    f(path + i)
+
+        res = []
+        f("")
+        return res
