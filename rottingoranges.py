cdc529ecb1bdad89c779447d14711b62fa233fdf
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-20 12:19:43 -0400

    Update rottingoranges.py

--- rottingoranges.py
+++ rottingoranges.py
@@ -958,3 +958,32 @@ class Solution:
                     d.append([destroyx, destroyy])
             minminutes += 1
         return minminutes if freshcount == 0 else -1
+
+
+#3/20/24:
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        d = deque() 
+        freshcount, minminutes = 0, 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 2:
+                    d.append([r, c])
+                elif grid[r][c] == 1:
+                    freshcount += 1
+                else:
+                    continue 
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        while d and freshcount > 0:
+            for i in range(len(d)): #each minute
+                rottenx, rotteny = d.popleft()
+                for newx, newy in directions:
+                    destroyx, destroyy = rottenx + newx, rotteny + newy
+                    if destroyx < 0 or destroyx >= len(grid) or destroyy < 0 or destroyy >= len(grid[0]) or grid[destroyx][destroyy] != 1:
+                        continue
+                    grid[destroyx][destroyy] = 2
+                    freshcount -= 1
+                    d.append([destroyx, destroyy])
+            minminutes += 1
+        return minminutes if freshcount == 0 else -1
