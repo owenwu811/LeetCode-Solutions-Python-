109ceeb06201bc361f200cd2b182ef26573e3bb1
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-20 20:07:10 -0400

    Update addbinary.py

--- addbinary.py
+++ addbinary.py
@@ -70,4 +70,11 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         s = bin(int(a, 2) + int(b, 2))
         return s[2:]
-        
+
+#show work:
+#a = "11", b = "1"
+# 11 > 2 ^ 0 + 2 ^ 1 = 3
+# 1 > 2 ^ 0 = 1
+# 3 + 1 = 4
+# print(bin(4)) > 0b100
+
