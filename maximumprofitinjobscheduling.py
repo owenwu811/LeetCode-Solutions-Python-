dc6ed9001d4cf721b05747402c7b1cbc9b4579b8
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-22 22:15:13 -0400

    Update maximumprofitinjobscheduling.py

--- maximumprofitinjobscheduling.py
+++ maximumprofitinjobscheduling.py
@@ -241,3 +241,22 @@ class Solution:
 
 
         return dfs(0)
+
+
+#3/22/24 practice:
+
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        intervals = sorted(zip(startTime, endTime, profit))
+        cache = {}
+        def dfs(i):
+            if i in cache:
+                return cache[i]
+            if i >= len(intervals):
+                return 0
+            res = dfs(i + 1)
+            j = bisect.bisect(intervals, (intervals[i][1], -1, -1))
+            cache[i] = max(res, intervals[i][2] + dfs(j))
+            res = max(res, intervals[i][2] + dfs(j))
+            return res
+        return dfs(0)
