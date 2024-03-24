74af244d287ac378838f2591fcb4772965530142
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-24 10:21:21 -0400

    Update trappingrainwater.py

--- trappingrainwater.py
+++ trappingrainwater.py
@@ -250,3 +250,23 @@ class Solution:
                 maxr = max(maxr, height[r])
                 res += (maxr - height[r])
         return res
+
+#3/24/24:
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        #non negative means 0 or positive while positive means above 0 not including 0
+        if not height: return 0
+        res = 0
+        l, r = 0, len(height) - 1
+        maxleft, maxright = height[l], height[r]
+        while l < r: #no = because can't trap water at highest elevation
+            if maxleft < maxright:
+                l += 1 #getting rid of shorter constraint
+                maxleft = max(maxleft, height[l])
+                res += (maxleft - height[l])
+            else:
+                r -= 1
+                maxright = max(maxright, height[r])
+                res += (maxright - height[r])
+        return res
