432e27eae2d7f0f90be07a40cd3aa5a591852207
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-24 18:31:13 -0400

    Update jumpgame.py

--- jumpgame.py
+++ jumpgame.py
@@ -1,37 +1,21 @@
 
-5. Jump Game
-Medium
-18.1K
-1K
-Companies
-You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+#You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
 
-Return true if you can reach the last index, or false otherwise.
+#Return true if you can reach the last index, or false otherwise.
 
+
+
+#Example 1:
+
+#Input: nums = [2,3,1,1,4]
+#Output: true
+#Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+#Example 2:
+
+#Input: nums = [3,2,1,0,4]
+#Output: false
+#Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
  
-
-Example 1:
-
-Input: nums = [2,3,1,1,4]
-Output: true
-Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
-Example 2:
-
-Input: nums = [3,2,1,0,4]
-Output: false
-Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
- 
-
-Constraints:
-
-1 <= nums.length <= 104
-0 <= nums[i] <= 105
-Accepted
-1.6M
-Submissions
-4.2M
-Acceptance Rate
-38.6%
 
 
 Python Solution :
@@ -43,4 +27,17 @@ class Solution:
             if i > reachable:
                 return False
             reachable = max(reachable, i + nums[i])
+        return True
+
+#3/24/24 refresher (missed):
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        #reachable variable represents the farthest index that can be reached from previous positions but is updated based on the current position (the max reachable index from all positions processed so far)
+        reachable = 0
+        for i in range(len(nums)):
+            if i > reachable: #remember that i is always <= the last index, so if i is bigger than reachable up to the current position, we can already return False
+                return False
+            #adding i to nums[i] gives us the index that can be reached by taking these steps. 
+            reachable = max(reachable, i + nums[i]) #nums[i] = number of steps we can take from current position i. if nums[i] = 3, we can take 3 steps forward from index i. 
         return True
