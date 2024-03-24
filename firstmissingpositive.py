7ead1945f1f3856e32e22fd7c045a63a61a4d203
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-24 09:49:49 -0400

    Update firstmissingpositive.py

--- firstmissingpositive.py
+++ firstmissingpositive.py
@@ -22,3 +22,16 @@ class Solution:
 
 #we need nums.sort() or else we will fail test case nums = [2, 1] because we will return 2 instead of 3 because 0 dosen't count as a missing positive integer 
 #the purpose of sorting is to ensure that we are going up by increments of 1 as we iterate right in the input array
+
+#3/24/24:
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums.sort()
+        res = 1
+        for n in nums:
+            if n <= 0:
+                continue
+            elif res == n:
+                res += 1
+        return res
