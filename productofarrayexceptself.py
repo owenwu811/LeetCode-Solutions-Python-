5b5975ed45ad5bc60d086b38963b6acd54c9fb60
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-24 23:08:45 -0400

    Update productofarrayexceptself.py

--- productofarrayexceptself.py
+++ productofarrayexceptself.py
@@ -349,3 +349,17 @@ class Solution:
             res[backward] *= postfixn
             postfixn *= nums[backward]
         return res
+
+#3/24/24:
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [0] * len(nums)
+        prefixn, postfixn = 1, 1
+        for forward in range(len(nums)):
+            res[forward] = prefixn
+            prefixn *= nums[forward]
+        for backward in range(len(nums) -1, -1, -1):
+            res[backward] *= postfixn
+            postfixn *= nums[backward]
+        return res
