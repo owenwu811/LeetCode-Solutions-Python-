86899be55baa486133f87e912fdf9676b3a0fe26
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-25 22:29:44 -0400

    Update DecodeString.py

--- DecodeString.py
+++ DecodeString.py
@@ -115,3 +115,24 @@ class Solution:
                     k = stack.pop() + k #build consecutive digit
                 stack.append(int(k) * substr)
         return "".join(stack)
+
+
+#3/25/24 practice:
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if char != "]":
+                stack.append(char)
+            else:
+                substr = ""
+                while stack and stack[-1] != "[":
+                    substr = stack.pop() + substr
+                stack.pop()
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+                stack.append(int(k) * substr)
+        return "".join(stack)
+        
