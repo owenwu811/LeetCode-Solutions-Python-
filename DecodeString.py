63f8a45cdf972a7d82332a0211f1c3aed24a906b
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-26 19:45:09 -0400

    Update DecodeString.py

--- DecodeString.py
+++ DecodeString.py
@@ -136,3 +136,22 @@ class Solution:
                 stack.append(int(k) * substr)
         return "".join(stack)
         
+
+#3/26/24:
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for char in s:
+            if char != "]":
+                stack.append(char)
+            else:
+                substr = ""
+                while stack and stack[-1] != "[":
+                    substr = stack.pop() + substr
+                stack.pop() 
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+                stack.append(int(k) * substr)
+        return "".join(stack)
