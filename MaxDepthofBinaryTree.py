8c21d76d25b913a855b9a4cddd5d0b6a3d2b86fe
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-26 14:00:47 -0400

    Update MaxDepthofBinaryTree.py

--- MaxDepthofBinaryTree.py
+++ MaxDepthofBinaryTree.py
@@ -132,3 +132,19 @@ class Solution:
         l = self.maxDepth(root.left)
         r = self.maxDepth(root.right)
         return 1 + max(l, r)
+
+#3/26/24: (don't overthink - this is just traversing down both left and right and finding the bigger of the two!)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        l = self.maxDepth(root.left)
+        r = self.maxDepth(root.right)
+        return 1 + max(l, r)
