9603717f7c04849b0950cd8a946fbfed13c0629c
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-31 18:33:46 -0400

    Update slidingwindowmaximum.py

--- slidingwindowmaximum.py
+++ slidingwindowmaximum.py
@@ -11,21 +11,19 @@
 
 class Solution:
    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        #monotonically decreasing queue
-        #size of our window is k
-        #we want to append the maximum number from each window of size k to an array and return the array
         output = []
         q = deque()
         l = 0
         for r in range(len(nums)):
-            #pop smaller values from queue
+            #before adding 5 to our deque, we must check if 5 is greater than the value at top of deque. if it is, since 5 > 4, then don't consider 4 as max value ever again = [1, 1, 1, 1, 1, 4, 5], k = 6
             while q and nums[q[-1]] < nums[r]: 
-                q.pop() 
-            q.append(r) 
-            if l > q[0]:
-                q.popleft()
-            if (r + 1) >= k:
-                output.append(nums[q[0]])
+                q.pop() #since we don't consider 4 as max value ever again since 5 > 4, pop 4 from deque
+            q.append(r) #add 5 to the top 
+            if l > q[0]: #[8, 7, 6, 9], k = 2. deque = [7, 6]. our window is [6, 9], so since 7 is no longer in bounds, and max value in our window is 7, we add 7 to the output before we pop 7 from our deque
+                q.popleft() #the action of popping 7 from our deque
+            if (r + 1) >= k: 
+                output.append(nums[q[0]]) #since our deque is always in decreasing order [8, 7], we can look at the leftmost value in our deque [8, 7] - 8 - and add 8 (leftmost) to our output
                 l += 1
             r += 1
         return output
+
