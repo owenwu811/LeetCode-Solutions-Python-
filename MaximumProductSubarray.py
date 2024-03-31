ddb15cb5fb0776d3d7f3b10d09310ac4ccc83e22
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-31 10:50:36 -0400

    Update MaximumProductSubarray.py

--- MaximumProductSubarray.py
+++ MaximumProductSubarray.py
@@ -84,3 +84,18 @@ class Solution:
             res = max(res, curmax)
         return res
 
+#3/31/24:
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        curmin, curmax = 1, 1
+        for n in nums:
+            if n == 0:
+                curmin, curmax = 1, 1
+                continue
+            temp = curmax * n
+            curmax = max(n * curmax, n * curmin, n)
+            curmin = min(n * curmin, temp, n)
+            res = max(res, curmax, curmin)
+        return res
