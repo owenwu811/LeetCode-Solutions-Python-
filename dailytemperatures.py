4b98a6f0dcd3ee89d5ef88788243b5597fc7d844
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-03-31 10:48:58 -0400

    Update dailytemperatures.py

--- dailytemperatures.py
+++ dailytemperatures.py
@@ -43,3 +43,16 @@ class Solution:
                 res[a] = i - a
             stack.append(i)
         return res
+
+#3/31/24:
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = []
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                a = stack.pop()
+                res[a] = i - a
+            stack.append(i)
+        return res
