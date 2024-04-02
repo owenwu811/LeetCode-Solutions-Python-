e30324fff964e7564cd09051214c7c80449271a5
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-02 12:27:12 -0400

    Update slidingwindowmaximum.py

--- slidingwindowmaximum.py
+++ slidingwindowmaximum.py
@@ -86,3 +86,21 @@ class Solution:
                 res.append(nums[d[0]])
                 l += 1
         return res
+
+#4/2/24:
+
+class Solution:
+   def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = []
+        l = 0
+        d = deque()
+        for r in range(len(nums)):
+            while d and nums[r] > nums[d[-1]]:
+                d.pop() #useless because smaller
+            d.append(r)
+            if l > d[0]:
+                d.popleft()
+            if (r + 1) >= k:
+                res.append(nums[d[0]])
+                l += 1
+        return res
