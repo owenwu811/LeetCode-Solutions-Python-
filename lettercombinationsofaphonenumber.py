5fd17f34a136d616c270887293905cf2847d2ba4
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-03 21:29:11 -0400

    Update lettercombinationsofaphonenumber.py

--- lettercombinationsofaphonenumber.py
+++ lettercombinationsofaphonenumber.py
@@ -317,3 +317,20 @@ class Solution:
         res = []
         f("")
         return res
+
+#4/3/24 refresher:
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0: return []
+        mapping = {"2": ['a', 'b', 'c'], "3": ['d', 'e', 'f'], "4": ['g', 'h', 'i'], "5": ['j', 'k', 'l'], "6": ['m', 'n', 'o'], "7": ['p', 'q', 'r', 's'], "8": ['t', 'u', 'v'], "9": ['w', 'x', 'y', 'z']}
+        def f(path):
+            if len(path) == len(digits):
+                res.append(path)
+                return
+            for i in mapping[digits[len(path)]]: #note that without else works too!
+                f(path + i)
+        
+        res = []
+        f("")
+        return res
