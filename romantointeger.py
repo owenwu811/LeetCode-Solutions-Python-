b3f9f12ab34ee59566ea22cbd69387767e5724d7
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-05 13:30:31 -0400

    Update romantointeger.py

--- romantointeger.py
+++ romantointeger.py
@@ -32,3 +32,16 @@ class Solution:
             else:
                 res += mydict[s[char]]
         return res
+
+#4/5/24:
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        mydict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        res = 0
+        for i in range(len(s)):
+            if i < len(s) - 1 and mydict[s[i]] < mydict[s[i + 1]]:
+                res -= mydict[s[i]]
+            else:
+                res += mydict[s[i]]
+        return res
