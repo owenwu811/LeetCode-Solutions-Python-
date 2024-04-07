77bb7a8bb885a34fea0d3307699dd4a8d893988f
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-07 18:34:10 -0400

    Update uniquepaths.py

--- uniquepaths.py
+++ uniquepaths.py
@@ -409,3 +409,16 @@ class Solution:
                 buildoff[right] = buildoff[right + 1] + first[right]
             first = buildoff
         return first[0]
+
+#4/7/24:
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        first = [1] * n
+        for down in range(m - 1):
+            above = [1] * n
+            for right in range(n - 2, -1, -1):
+                above[right] = above[right + 1] + first[right]
+            first = above
+        return first[0]
+
