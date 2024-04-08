13ae4729d195c50e0cdea0857a667f8820dcdcb6
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-07 20:04:47 -0400

    Update movezeros.py

--- movezeros.py
+++ movezeros.py
@@ -25,6 +25,6 @@ class Solution:
             if nums[slow] == 0 and nums[fast] != 0:
                 nums[slow], nums[fast] = nums[fast], nums[slow]
                 slow += 1
-            elif nums[slow] != 0:
+            elif nums[slow] != 0: #the reason we don't do any swapping if left points to a non zero is because left represents the point at which all zeros should be in front of left, so any non zero elements are already in their final place 
                 slow += 1
         
