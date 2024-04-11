ea37556d44b0b2bca1d3a96061dd7847c54ff06f
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-11 18:22:32 -0400

    Update removeNthnodefromendoflist.py

--- removeNthnodefromendoflist.py
+++ removeNthnodefromendoflist.py
@@ -16,10 +16,10 @@ class Solution:
         left = dummy
         right = head
         while n > 0 and right:
-            right = right.next
+            right = right.next #create gap between left and right of exactly n nodes 
             n -= 1
         while right: #the idea is to keep the distance between left and right exactly n nodes, and when right reaches none too far to the right of the linked list, delete the nth node with left by cutting off by pointing left.next to left.next.next to cut it, so if n = 2, remove 4 from [1, 2, 3, 4, 5]
             left = left.next
             right = right.next
-        left.next = left.next.next
+        left.next = left.next.next #cut off the nth node with left pointer
         return dummy.next
