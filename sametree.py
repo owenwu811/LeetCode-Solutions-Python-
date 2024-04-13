cbffbc8c111a6e031099e71edffa0477ce1613e8
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-13 09:24:10 -0400

    Update sametree.py

--- sametree.py
+++ sametree.py
@@ -38,3 +38,18 @@ tree1.left = TreeNode(2)
 # None 2
 
 #at some point, p == 2 and q == None, so if you did "if p == None and q == None", that would be False since p is not None, so the return (p.val == q.val) would execute, and would result in None typeerror because you are trying to call p.vall == q.val, and q.val is None, which dosen't have val
+
+#4/13/24:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        #dfs
+        if p == None or q == None:
+            return p == q
+        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
