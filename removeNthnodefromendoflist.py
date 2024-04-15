31d8d5e8599ea5c09d72325d254439f8242586e8
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-15 12:49:20 -0400

    Update removeNthnodefromendoflist.py

--- removeNthnodefromendoflist.py
+++ removeNthnodefromendoflist.py
@@ -108,3 +108,24 @@ class Solution:
             l = l.next
         l.next = l.next.next
         return dummy.next #the list is already with the nth element cut off, so return the head of the list, synonymous with the entire list at that point cause linked lists are pointers to the next node
+
+#4/15/24:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        l = dummy
+        r = head
+        while n > 0 and r:
+            r = r.next
+            n -= 1
+        while r:
+            l = l.next
+            r = r.next
+        l.next = l.next.next
+        return dummy.next
