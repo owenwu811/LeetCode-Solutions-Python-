b4289651015c336870d49bdea86da43fe7430256
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-16 08:49:24 -0400

    Update romantointeger.py

--- romantointeger.py
+++ romantointeger.py
@@ -117,3 +117,15 @@ class Solution:
         return res
         
         
+#4/16/24:
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        mapping = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        for i in range(len(s)):
+            if i > 0 and mapping[s[i]] > mapping[s[i - 1]]:
+                res -= mapping[s[i - 1]]
+            else:
+                res += mapping[s[i - 1]]
+        return res
