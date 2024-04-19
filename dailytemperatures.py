0a679c533b8ee4de7796eec2776a8739e132222b
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-19 18:40:02 -0400

    Update dailytemperatures.py

--- dailytemperatures.py
+++ dailytemperatures.py
@@ -111,3 +111,16 @@ class Solution:
             d.append(i)
         return res
 
+
+#4/19/24:
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        d = deque()
+        for i in range(len(temperatures)):
+            while d and temperatures[i] > temperatures[d[-1]]:
+                a = d.pop()
+                res[a] = i - a
+            d.append(i)
+        return res
