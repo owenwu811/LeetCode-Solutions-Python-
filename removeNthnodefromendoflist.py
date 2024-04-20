12d50869ed340268f994627b4bc9e2055e3663f0
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-20 09:48:16 -0400

    Update removeNthnodefromendoflist.py

--- removeNthnodefromendoflist.py
+++ removeNthnodefromendoflist.py
@@ -129,3 +129,25 @@ class Solution:
             r = r.next
         l.next = l.next.next
         return dummy.next
+
+#4/20/24:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        l = dummy
+        r = head
+        while r and n > 0:
+            r = r.next
+            n -= 1 #the difference between l and r pointers to be n LENGTH of nodes
+        while r:
+            l = l.next
+            r = r.next
+        l.next = l.next.next
+        return dummy.next
+
