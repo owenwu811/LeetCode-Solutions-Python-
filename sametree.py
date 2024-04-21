b29d7632b4004b926a7568b79061b1793ed67189
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-21 16:38:39 -0400

    Update sametree.py

--- sametree.py
+++ sametree.py
@@ -82,3 +82,17 @@ class Solution:
         if p == None or q == None: #if p == 2 and q == None, this inner block would exeute because or is true. if you used and, this if block would be false, and you would go to return (p.val == q.val), which would throw an error for p = 2 q = None for nontype
             return p == q
         return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+#4/21/24:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == None or q == None:
+            return p == q
+        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
