19c173b55cb9df84b96401e5f36f6b57449ab1f8
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-22 14:14:38 -0400

    Update sametree.py

--- sametree.py
+++ sametree.py
@@ -97,3 +97,21 @@ class Solution:
         if p == None or q == None:
             return p == q
         return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+
+#4/22/24:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#a better solution to handle p == [] q == [] because that should be True, not False
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q: return True
+        if p == None or q == None:
+            return p == q
+        return (p.val == q.val) and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
