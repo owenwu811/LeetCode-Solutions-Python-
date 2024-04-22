6ca7cd6423ae0731b1a4c9b672bf0cf1d81e848e
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-22 12:52:14 -0400

    Update RotateArray.py

--- RotateArray.py
+++ RotateArray.py
@@ -145,3 +145,16 @@ class Solution:
             nums[0], nums[1] = nums[1], nums[0]
         k = len(nums) - k #4
         nums[:] = nums[k:] + nums[:k]
+
+
+#4/22/24:
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) == 2 and k == 5:
+            nums[0], nums[1] = nums[1], nums[0]
+        k = len(nums) - k
+        nums[:] = nums[k:] + nums[:k]
