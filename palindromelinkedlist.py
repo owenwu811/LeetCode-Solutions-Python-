4068cacb43ca62081142228ff8c3764fe6afbaf4
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-23 09:26:11 -0400

    Update palindromelinkedlist.py

--- palindromelinkedlist.py
+++ palindromelinkedlist.py
@@ -11,3 +11,15 @@ class Solution:
             a.append(head.val)
             head = head.next
         return a == a[::-1]
+
+
+#4/23/24:
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        res = []
+        right = head
+        while right:
+            res.append(right.val)
+            right = right.next
+        return res == res[::-1]
