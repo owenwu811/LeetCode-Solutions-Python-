209d06768641f18e5ca34ff2a9aff58aa8787e07
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-23 23:35:24 -0400

    Update longestincreasingsubsequence.py

--- longestincreasingsubsequence.py
+++ longestincreasingsubsequence.py
@@ -17,3 +17,16 @@ class Solution:
                     res[i] = max(res[i], 1 + res[j])
         return max(res)
 
+
+#review again:
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        res = [1] * len(nums)
+        for i in range(len(nums) -1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    res[i] = max(res[i], 1 + res[j])
+        return max(res)
+
+
