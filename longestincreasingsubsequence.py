a9969059ef44e0894bd907dde4744e5b37878deb
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-23 23:48:31 -0400

    Update longestincreasingsubsequence.py

--- longestincreasingsubsequence.py
+++ longestincreasingsubsequence.py
@@ -30,3 +30,5 @@ class Solution:
         return max(res)
 
 
+#let's say you have [1, 3, 2, 4] - when i = 1 and j = 2, giving us 3 and 2 for i and j as values, 3 is not less than 2, so j moves forward, so do max(res[i], 1 + res[j]) because res[j] = 1 since from the last element, we only have one longest increasing length, but then the current i counts because index 1 is less than index 3, so add 1! this logic works for nums = [10,9,2,5,3,7,101,18] as well when 5 > 3, but 2 < 3, so we do 1 + res[3], and res[3] was already computed previously!
+
