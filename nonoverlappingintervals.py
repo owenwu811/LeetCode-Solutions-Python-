89070d7af59f4942fc69761c569d964083a4359b
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-24 21:53:33 -0400

    Update nonoverlappingintervals.py

--- nonoverlappingintervals.py
+++ nonoverlappingintervals.py
@@ -89,3 +89,18 @@ class Solution:
                 res += 1
                 prevend = min(prevend, end)
         return res
+
+#4/24/24 refresher:
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        res = 0
+        intervals.sort()
+        prevend = intervals[0][1]
+        for start, end in intervals[1:]:
+            if start >= prevend:
+                prevend = end
+            else:
+                res += 1
+                prevend = min(prevend, end)
+        return res
