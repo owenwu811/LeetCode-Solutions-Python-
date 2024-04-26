bc697d0876bcc3caefe1401ca9b1c011e10db7da
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-26 10:29:23 -0400

    Update symmetrictree.py

--- symmetrictree.py
+++ symmetrictree.py
@@ -76,3 +76,17 @@ class Solution:
 #     1
 #         2
 #     4
+
+#4/26/24 refresher:
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def solve(root1, root2):
+            if root1 == None and root2 == None:
+                return True
+            if root1 == None or root2 == None or root1.val != root2.val:
+                return False
+            return solve(root1.left, root2.right) and solve(root1.right, root2.left)
+
+
+        return solve(root, root)
