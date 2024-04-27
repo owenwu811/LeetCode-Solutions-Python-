435eb09730dd67478254592a6041b22588d9f90b
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-27 18:50:07 -0400

    Update jumpgame.py

--- jumpgame.py
+++ jumpgame.py
@@ -159,4 +159,14 @@ class Solution:
         return True
         
 
+#4/27/24 refresher:
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        reachable = 0
+        for i in range(len(nums)):
+            if i > reachable:
+                return False
+            reachable = max(reachable, i + nums[i])
+        return True
         
