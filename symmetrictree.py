a50803ab5f453ba450bb02047b24505027ce19fa
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-27 11:19:50 -0400

    Update symmetrictree.py

--- symmetrictree.py
+++ symmetrictree.py
@@ -90,3 +90,22 @@ class Solution:
 
 
         return solve(root, root)
+
+
+#4/27/24:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def solve(root1, root2):
+            if root1 == None and root2 == None:
+                return True
+            if root1 == None or root2 == None or root1.val != root2.val:
+                return False #one imbalance means entire thing is imbalanced
+            return solve(root1.left, root2.right) and solve(root1.right, root2.left)
+        return solve(root, root)
