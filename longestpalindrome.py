5da56bd389d70d6b9d041ae629cda564aff6bfb0
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-27 22:17:37 -0400

    Update longestpalindrome.py

--- longestpalindrome.py
+++ longestpalindrome.py
@@ -25,3 +25,6 @@ class Solution:
 #to illustrate, abccccdd > 1 a, 1 b, 4 cs, 2 ds, so we can either choose a OR b to plop in the middle, and the other odd has to be removed from input
 # aaabccccdd > 3 as, 1 b, 4 cs, 2 ds, so, again we can chose ONE OF THE 3 AS or JUST THE B to plop in the middle, and the all the other odds (2 a + 1 b or 3 a) need to be removed from input
 # dccaccd, so we are left with aab in the input, and no matter how you put aab inside of dccaccd, you will never form a palindrome - if you put the 2 as in front and the b in middle, you get adccbaccda or adccabccda, which are not palindromes because the b gets hit first or later when coming backwards vs. forwards, so you gotta remove 3 (aab or aaa) as length from input since we already tolerated one of the odds - 4 total - aaab = len(4), and we either removed ONE a or ONE b 
+
+
+#for "aaaccccdd", res is 9 (whole input) because you can do adccaccda, so the set is {'a'} at the end, and since we can tolerate one of the As by adding 1, we can plop the other 2 As at end, so we can tolerate one of the 3 As
