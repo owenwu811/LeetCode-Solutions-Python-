500fb7e79750dea89dd28100ba74abc87eab931e
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-04-29 13:28:18 -0400

    Update longestconsecutivesequence.py

--- longestconsecutivesequence.py
+++ longestconsecutivesequence.py
@@ -171,3 +171,18 @@ class Solution:
                 res = 0
             res += 1
         return max(maxres, res)
+
+#4/29/24 refresher:
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums.sort()
+        res, maxres = 0, 0
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            if i > 0 and nums[i] - nums[i - 1] > 1:
+                maxres = max(res, maxres)
+                res = 0
+            res += 1
+        return max(maxres, res)
