4702ab3def0a13cccf81d5220f8cb089b9892f99
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-03 18:14:21 -0400

    Update clonegraph.py

--- clonegraph.py
+++ clonegraph.py
@@ -357,3 +357,29 @@ class Solution:
             return copy
         return f(node) if node else None
 
+
+#5/3/24:
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None): 
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        self.d = {} #{o: c}
+        def f(node):
+            if node in self.d:
+                return self.d[node]
+            copy = Node(node.val)
+            self.d[node] = copy
+            for n in node.neighbors:
+                copy.neighbors.append(f(n))
+            return copy
+        return f(node) if node else None
+            
