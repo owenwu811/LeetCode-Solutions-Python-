24aca6e56cbbce483b7d6ec30cbde6db036b559c
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-04 10:23:12 -0400

    Update evalutatereversepolishnotation.py

--- evalutatereversepolishnotation.py
+++ evalutatereversepolishnotation.py
@@ -301,3 +301,30 @@ class Solution:
             else:
                 stack.append(int(token))
         return stack.pop()
+
+
+#5/4/24 refresher:
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token == "+":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(b + a))
+            elif token == "*":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(b * a))
+            elif token == "/":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(b / a))
+            elif token == "-":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(b - a))
+            else:
+                stack.append(int(token))
+        return stack[0]
