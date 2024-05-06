d6b86557930919b755485b5d9bee413d8da1ed84
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-06 13:01:32 -0400

    Update diameterofbinarytree.py

--- diameterofbinarytree.py
+++ diameterofbinarytree.py
@@ -240,3 +240,26 @@ class Solution:
         self.res = 0
         f(root)
         return self.res
+
+
+#5/6/24 missed:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        def f(root):
+            if root == None:
+                return 0
+            l = f(root.left)
+            r = f(root.right)
+            self.res = max(self.res, l + r) #without self, the res would always b 0 even if you passed it in because it wouldn't global!
+            return 1 + max(l, r)
+            
+        self.res = 0 #without self, the res would always b 0 even if you passed it in!
+        f(root)
+        return self.res #without self, the res would always b 0 even if you passed it in! you would just get the res declared outside of the recursive function!
