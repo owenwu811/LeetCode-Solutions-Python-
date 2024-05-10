df9608b84380e28c2a30c2e36bfc017d9a7a4a5e
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-09 21:06:01 -0400

    Create countingbits.py

---
+++ countingbits.py
@@ -0,0 +1,20 @@
+
+#Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
+
+#Input: n = 2
+#Output: [0,1,1]
+#Explanation:
+#0 --> 0
+#1 --> 1
+#2 --> 10
+
+#my own solution in python3:
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+        for i in range(n + 1):
+            cnt = bin(i).count('1')
+            res.append(cnt)
+        return res
