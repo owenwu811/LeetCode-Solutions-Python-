e8477c9e1dc4dd45848848761509f67dee693e00
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-09 20:57:33 -0400

    Create numberofonebits.py

---
+++ numberofonebits.py
@@ -0,0 +1,10 @@
+
+#Write a function that takes the binary representation of a positive integer and returns the number of 
+#set bits it has (also known as the Hamming weight).
+
+
+#python3 solution:
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        return bin(n).count('1')
