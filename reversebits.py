d3027d03fed2f7ad7371fb5bea6de6b1e7178aa3
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-12 21:43:54 -0400

    Update reversebits.py

--- reversebits.py
+++ reversebits.py
@@ -7,6 +7,7 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
         for i in range(32): #an integer in python has 32 bits 
+            #(n & 1) gets the rightmost number, and then result << 1 places that number at the front, so this below line is just a way to reverse the bits 
             result = (result << 1) | (n & 1) # (10 << 2) just adds 2 more 0s to the end of the binary representation of 10
             n //= 2
         return result
