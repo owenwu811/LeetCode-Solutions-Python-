89bf684a547772c6036b9c735796f642d679b3b0
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-13 10:18:16 -0400

    Update medianoftwosortedarrays.py

--- medianoftwosortedarrays.py
+++ medianoftwosortedarrays.py
@@ -243,3 +243,31 @@ class Solution:
             else:
                 l = i + 1
 
+
+#5/13/24 refresher:
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        a, b = nums1, nums2
+        total = len(nums1) + len(nums2)
+        totalhalf = total // 2
+        if len(b) < len(a):
+            a, b = b, a
+        l, r = 0, len(a) - 1
+        while True:
+            i = (l + r) // 2
+            j = totalhalf - i - 2
+            aleft = a[i] if i >= 0 else float('-inf')
+            aright = a[i + 1] if (i + 1) < len(a) else float('inf') #true for a[1] b[2, 3]
+            bleft =  b[j] if j >= 0 else float('-inf')
+            bright = b[j + 1] if (j + 1) < len(b) else float('inf')
+            if aleft <= bright and bleft <= aright:
+                if total % 2 > 0:
+                    return min(aright, bright)
+                return (min(aright, bright) + max(aleft, bleft)) / 2
+            elif aleft > bright:
+                r = i - 1
+            else:
+                l = i + 1
+
+
