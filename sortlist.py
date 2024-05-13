569c4864ecf2e6ee1e7f96e08b0efc48f72eee2e
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-13 12:56:45 -0400

    Update sortlist.py

--- sortlist.py
+++ sortlist.py
@@ -80,3 +80,37 @@ class Solution:
             tail.next = list2
         return dummy.next
 
+
+#5/13/24 refresher:
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        left, right = head, self.getMid(head)
+        tmp = right.next
+        right.next = None
+        right = tmp
+        left, right = self.sortList(left), self.sortList(right)
+        return self.merge(left, right)
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    def merge(self, list1, list2):
+        dummy = tail = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1 and not list2:
+            tail.next = list1
+        elif list2 and not list1:
+            tail.next = list2
+        return dummy.next
