61d6e2579c6f70e1fcb3a6b030812ae896ceaa29
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-16 15:03:50 -0400

    Update spiralmatrix.py

--- spiralmatrix.py
+++ spiralmatrix.py
@@ -392,3 +392,24 @@ class Solution(object):
                 newr, newc = r + directions[d][0], c + directions[d][1]
             r, c = newr, newc
         return res
+
+
+#5/16/24 refresher:
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        seen = set()
+        r = c = d = 0
+        rows, cols = len(matrix), len(matrix[0])
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        for i in range(rows * cols):
+            seen.add((r, c))
+            res.append(matrix[r][c])
+            newr, newc = r + directions[d][0], c + directions[d][1]
+            if newr < 0 or newr >= len(matrix) or newc < 0 or newc >= len(matrix[0]) or (newr, newc) in seen:
+                d = (d + 1) % 4
+                newr, newc = r + directions[d][0], c + directions[d][1]
+            r, c = newr, newc
+        return res
+            
