34a24e3574834e25a673c5de3b8fdd76932ccc77
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-15 21:14:45 -0400

    Update convertsortedarraytoBST.py

--- convertsortedarraytoBST.py
+++ convertsortedarraytoBST.py
@@ -123,3 +123,25 @@ class Solution:
             root.right = f(mid + 1, r) #build right subtree
             return root #return the entire tree
         return f(0, len(nums) - 1) #return the result of the recursion aka the entire tree
+
+
+#5/15/24 refresher:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        def f(l, r):
+            if l > r:
+                return None
+            mid = (l + r) // 2
+            root = TreeNode(nums[mid])
+            root.left = f(l, mid - 1)
+            root.right = f(mid + 1, r)
+            return root
+        return f(0, len(nums) - 1)
+
