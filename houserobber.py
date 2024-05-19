57a28fb2e86869f6d80c9e3ee1de7723e797ebe6
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-18 22:02:16 -0400

    Update houserobber.py

--- houserobber.py
+++ houserobber.py
@@ -131,3 +131,16 @@ class Solution:
             rob1 = rob2 #rob1 = rob2, which is 0 in the 1st iteration
             rob2 = tempmax
         return rob2
+
+#5/18/24 refresher:
+
+class Solution:
+    import math
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        for n in nums:
+            tmp = max(rob1 + n, rob2)
+            rob1 = rob2
+            rob2 = tmp
+        return rob2
+
