f87693dfd1d3817993b6a7abd82c6222a5138dd7
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-19 19:45:00 -0400

    Update insertinterval.py

--- insertinterval.py
+++ insertinterval.py
@@ -217,3 +217,19 @@ class Solution:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         res.append(newInterval)
         return res
+
+#5/19/24 (really struggled with and missed - had to look at solution):
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        for i in range(len(intervals)):
+            if intervals[i][1] < newInterval[0]: #no overlap - REMEMBER THAT THE TWO IF CONDITIONALS COVER CASES WHERE NO OVERLAP 
+                res.append(intervals[i])
+            elif newInterval[1] < intervals[i][0]: #no overlap - REMEMBER 
+                res.append(newInterval)
+                return res + intervals[i:]
+            else: #there is overlap
+                newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+        res.append(newInterval)
+        return res
