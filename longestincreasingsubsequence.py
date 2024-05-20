f2140edef728cd004a79512ab88ec778b96cbda7
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-19 21:55:09 -0400

    Update longestincreasingsubsequence.py

--- longestincreasingsubsequence.py
+++ longestincreasingsubsequence.py
@@ -29,6 +29,7 @@ class Solution:
                     res[i] = max(res[i], 1 + res[j]) #[0, 1, 0, 3, 2, 3] - at index 4, when i = 4 and j = 5, 2 < 4, so res[4] (res[i] not res[j]) becomes 2. similarly, when i = 2 and j = 3, 0 < 3, so res[2] (res[i]) becomes 2
         return max(res)
 
+#
 
 #let's say you have [1, 3, 2, 4] - when i = 1 and j = 2, giving us 3 and 2 for i and j as values, 3 is not less than 2, so j moves forward, so do max(res[i], 1 + res[j]) because res[j] = 1 since from the last element, we only have one longest increasing length, but then the current i counts because index 1 is less than index 3, so add 1! this logic works for nums = [10,9,2,5,3,7,101,18] as well when 5 > 3, but 2 < 3, so we do 1 + res[3], and res[3] was already computed previously!
 
@@ -91,3 +92,13 @@ class Solution:
                     res[j] = max(res[j], 1 + res[i])
         return max(res)
 
+#5/19/24 practice:
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        res = [1] * len(nums)
+        for i in range(len(nums) -1, -1, -1):
+            for j in range(i, -1, -1):
+                if nums[j] < nums[i]:
+                    res[j] = max(res[j], 1 + res[i])
+        return max(res)
