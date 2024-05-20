a34c9d0d48feea911741916cc3d4afd1b36053c9
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-20 17:05:26 -0400

    Update Searchfora2dmatrix.py

--- Searchfora2dmatrix.py
+++ Searchfora2dmatrix.py
@@ -17,3 +17,29 @@ class Solution:
                 if target == matrix[i][j]:
                     return True
         return False
+
+
+#5/20/24 (missed) but my own solution in python3:
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        l, r = 0, len(matrix) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if matrix[mid][0] <= target <= matrix[mid][-1]: #we need to perform binary search in each row of the list of lists
+                row = matrix[mid]
+                lr, rr = 0, len(row) - 1
+                while lr <= rr:
+                    midrow = (lr + rr) // 2
+                    if row[midrow] < target:
+                        lr = midrow + 1
+                    elif row[midrow] > target:
+                        rr = midrow - 1
+                    else:
+                        return True
+                return False
+            elif matrix[mid][0] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return False
