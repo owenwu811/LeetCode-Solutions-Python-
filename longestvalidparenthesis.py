e5b70f5af639e0873c205d23801f04cc5a3d3c41
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-19 21:23:50 -0400

    Update longestvalidparenthesis.py

--- longestvalidparenthesis.py
+++ longestvalidparenthesis.py
@@ -194,3 +194,21 @@ class Solution:
                 else:
                     res = max(res, i - stack[-1])
         return res
+
+
+#5/19/24:
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        res = 0
+        stack = [-1]
+        for i, char in enumerate(s): #012"
+            if char == "(":
+                stack.append(i) 
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    res = max(res, i - stack[-1])
+        return res
