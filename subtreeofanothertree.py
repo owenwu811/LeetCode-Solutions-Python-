7ee433596963f13a8b1efb42def1735697988859
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-21 19:50:36 -0400

    Update subtreeofanothertree.py

--- subtreeofanothertree.py
+++ subtreeofanothertree.py
@@ -138,3 +138,25 @@ class Solution(object):
             return self.solve(p.left, q.left) and self.solve(p.right, q.right)
         return False
 
+
+#5/21/24 practice:
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        if not subRoot: return True
+        if not root: return False
+        if self.solve(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    def solve(self, p, q):
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.solve(p.left, q.left) and self.solve(p.right, q.right)
+        return False
