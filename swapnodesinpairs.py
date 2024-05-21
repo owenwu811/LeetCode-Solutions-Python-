00991eb98901861b0c54fc57f2de7ba27f68ef86
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-21 10:26:40 -0400

    Update swapnodesinpairs.py

--- swapnodesinpairs.py
+++ swapnodesinpairs.py
@@ -152,4 +152,26 @@ class Solution:
         return dummy.next
 
 
+#5/21/24:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        prev = dummy
+        cur = head
+        while cur and cur.next:
+            nxt = cur.next.next
+            rightone = cur.next
+            rightone.next = cur
+            cur.next = nxt
+            prev.next = rightone
+            prev = cur
+            cur = nxt
+        return dummy.next
+        
     
