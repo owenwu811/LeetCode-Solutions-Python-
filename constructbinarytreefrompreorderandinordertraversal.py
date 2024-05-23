daf4548b2e3bf9673cba93cba67c46f3c65d03be
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-23 10:04:23 -0400

    Update constructbinarytreefrompreorderandinordertraversal.py

--- constructbinarytreefrompreorderandinordertraversal.py
+++ constructbinarytreefrompreorderandinordertraversal.py
@@ -266,3 +266,22 @@ class Solution:
         root.left = self.buildTree(preorder[1: mid + 1], inorder[:mid])
         root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
         return root
+
+
+#5/23/24:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return 
+        root = ListNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+        return root
