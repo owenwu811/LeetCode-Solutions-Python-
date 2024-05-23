d5bfc901a893c315ba201c53473cde997e4848ce
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-23 09:56:50 -0400

    Update addtwonumbers.py

--- addtwonumbers.py
+++ addtwonumbers.py
@@ -31,3 +31,27 @@ class Solution:
             l1 = l1.next if l1 else None #in last iteration, l1 = None
             l2 = l2.next if l2 else None #in last iteration, l2 = None
         return dummy.next
+
+#5/23/24:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        cur = dummy
+        carry = 0
+        while l1 or l2 or carry > 0:
+            l1val = l1.val if l1 else 0
+            l2val = l2.val if l2 else 0
+            val = l1val + l2val + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
