0cbfcc7659f77082b9160030efe0a560fe00ddf4
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-23 22:27:25 -0400

    Update 01matrix.py

--- 01matrix.py
+++ 01matrix.py
@@ -502,3 +502,24 @@ class Solution:
                         d.append([ox, oy])
         return mat
 
+#5/23/24 refresher:
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        d = deque()
+        for r in range(len(mat)):
+            for c in range(len(mat[0])):
+                if mat[r][c] == 0:
+                    d.append([r, c])
+                else:
+                    mat[r][c] = float('inf')
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        while d:
+            for i in range(len(d)):
+                zx, zy = d.popleft()
+                for nx, ny in directions:
+                    ox, oy = zx + nx, zy + ny
+                    if ox >= 0 and ox < len(mat) and oy >= 0 and oy < len(mat[0]) and mat[ox][oy] > mat[zx][zy] + 1:
+                        mat[ox][oy] = mat[zx][zy] + 1
+                        d.append([ox, oy])
+        return mat
