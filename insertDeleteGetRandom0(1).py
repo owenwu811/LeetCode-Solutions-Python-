7707cb34c6115f3589aa9fe0b815221fb2c7212b
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-26 18:08:51 -0400

    Update insertDeleteGetRandom0(1).py

--- insertDeleteGetRandom0(1).py
+++ insertDeleteGetRandom0(1).py
@@ -130,3 +130,39 @@ class RandomizedSet:
 
     def getRandom(self) -> int:
         return random.choice(self.mylist)
+
+
+#practice again:
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.mydict = dict()
+        self.mylist = []
+
+        
+
+    def insert(self, val: int) -> bool:
+        if val not in self.mydict:
+            self.mylist.append(val)
+            self.mydict[val] = len(self.mylist) - 1
+            return True
+        return False
+        
+
+    def remove(self, val: int) -> bool:
+        if val in self.mydict:
+             #[1, 2] > we want to remove 1 because val = 1
+            rear = self.mylist[-1] #2
+            indextoinsert = self.mydict[val] #0
+            self.mylist[indextoinsert] = rear #
+            self.mylist.pop() #[1, 2] > [1]
+            self.mydict[rear] = indextoinsert #{2: 1} > {2: 0}
+            del self.mydict[val]
+            return True
+        return False
+
+        
+
+    def getRandom(self) -> int:
+        return random.choice(self.mylist)
