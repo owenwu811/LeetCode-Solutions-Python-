253297cb60dc744172a2dffc895c12c38a0506a4
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-26 11:20:53 -0400

    Update maximumfrequencystack.py

--- maximumfrequencystack.py
+++ maximumfrequencystack.py
@@ -78,4 +78,31 @@ class FreqStack:
             self.maxFreq -= 1
         return res
 
+
+#5/26/24 review:
+
+class FreqStack:
+    def __init__(self):
+        self.freq = defaultdict(int)
+        self.groups = defaultdict(list)
+        self.maxFreq = 0
+
+
+    def push(self, val: int) -> None:
+        self.freq[val] += 1
+        if self.freq[val] > self.maxFreq:
+            self.maxFreq = self.freq[val]
+        #{freq: [values]}
+        self.groups[self.freq[val]].append(val)
+
+
+    
+    def pop(self) -> int:
+        first = self.groups[self.maxFreq].pop() 
+        self.freq[first] -= 1
+        if not self.groups[self.maxFreq]:
+            self.maxFreq -= 1
+        return first
+
+
         
