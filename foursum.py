0daa2c85382cb93d81084d4c1b1a87971f2db75b
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-26 23:26:46 -0400

    Update foursum.py

--- foursum.py
+++ foursum.py
@@ -202,3 +202,35 @@ class Solution:
                         while nums[fourth] == nums[fourth + 1] and third < fourth:
                             fourth -= 1
         return res
+
+
+#5/26/24 review:
+
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+        for first in range(len(nums) - 3):
+            if first > 0 and nums[first] == nums[first - 1]:
+                continue
+            for second in range(first + 1, len(nums) - 2): #first + 1, not first!
+                if second > first + 1 and nums[second] == nums[second - 1]:
+                    continue 
+                third = second + 1
+                fourth = len(nums) - 1
+                while third < fourth:
+                    foursum = nums[first] + nums[second] + nums[third] + nums[fourth]
+                    if foursum < target:
+                        third += 1
+                    elif foursum > target:
+                        fourth -= 1
+                    else:
+                        res.append([nums[first], nums[second], nums[third], nums[fourth]])
+                        third += 1
+                        fourth -= 1
+                        while nums[third] == nums[third - 1] and third < fourth:
+                            third += 1
+                        while nums[fourth] == nums[fourth + 1] and third < fourth:
+                            fourth -= 1
+        return res
+
