93c66e33672f9e5c0f449f5d5d22b8464d626f26
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-27 09:33:47 -0400

    Update setmatrixzeros.py

--- setmatrixzeros.py
+++ setmatrixzeros.py
@@ -28,3 +28,25 @@ class Solution:
             for c in range(len(matrix[0])):
                 if matrix[r][c] == "*":
                     matrix[r][c] = 0
+
+#5/27/24 review:
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows, cols = len(matrix), len(matrix[0])
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == 0:
+                    for i in range(cols):
+                        if matrix[r][i] != 0:
+                            matrix[r][i] = "*"
+                    for i in range(rows):
+                        if matrix[i][c] != 0:
+                            matrix[i][c] = "*"
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == "*":
+                    matrix[r][c] = 0
