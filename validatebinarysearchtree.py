03615d3d21906e826c7282c9f37a4109acbe5bab
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-27 18:23:50 -0400

    Update validatebinarysearchtree.py

--- validatebinarysearchtree.py
+++ validatebinarysearchtree.py
@@ -490,4 +490,25 @@ class Solution:
             return f(lower, root.val, root.left) and f(root.val, upper, root.right)
 
         return f(float('-inf'), float('inf'), root)
+
+
+#5/27/24 review:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def f(lower, upper, root):
+            if not root:
+                return True
+            if not (lower < root.val < upper):
+                return False
+            return f(lower, root.val, root.left) and f(root.val, upper, root.right)
+        return f(float('-inf'), float('inf'), root)
+            
+            
             
