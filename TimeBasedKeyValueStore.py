53b98dcc9dd122de4ab954c0e596c5a60e8c04b9
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-28 10:43:27 -0400

    Update TimeBasedKeyValueStore.py

--- TimeBasedKeyValueStore.py
+++ TimeBasedKeyValueStore.py
@@ -647,3 +647,28 @@ class TimeMap:
             else:
                 r = mid - 1
         return res
+
+#5/28/24 review:
+
+class TimeMap:
+    def __init__(self):
+        self.d = dict()
+    
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.d:
+            self.d[key] = [] #[[]] for a[mid][1] to work, not []!
+        self.d[key].append([value, timestamp]) #making the [[]]
+
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        a = self.d.get(key, [])
+        l, r = 0, len(a) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if a[mid][1] <= timestamp:
+                res = a[mid][0]
+                l = mid + 1 #we want to find the largest that dosen't exceed timstamp_prev
+            else:
+                r = mid - 1
+        return res
