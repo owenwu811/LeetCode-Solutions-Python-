d2c5ea83c5fffbddda4fdafaa9b0526aabac7c41
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-28 16:08:15 -0400

    Update swapnodesinpairs.py

--- swapnodesinpairs.py
+++ swapnodesinpairs.py
@@ -195,6 +195,27 @@ class Solution:
             prev = cur #move prev and cur pointers two steps forward to the next pair to be swapped 
             cur = nxt
         return dummy.next
-        
+
+#another solution that makes more sense:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        lst = []
+        while head:
+            lst.append(head.val)
+            head = head.next
+        for i in range(0,len(lst)-1,2):
+            lst[i],lst[i+1]=lst[i+1],lst[i]
+        a = ListNode(0)
+        temp = a
+        for i in lst:
+            temp.next = ListNode(i)
+            temp = temp.next
+        return a.next
 
 
