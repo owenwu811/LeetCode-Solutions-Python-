9e765b15d1337ebe5589f858f3c7a82c899f0b87
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-30 11:33:25 -0400

    Update setmatrixzeros.py

--- setmatrixzeros.py
+++ setmatrixzeros.py
@@ -152,6 +152,27 @@ class Solution:
 
 
 
+#5/30/24 review:
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows, cols = len(matrix), len(matrix[0])
+        for r in range(len(matrix)):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == 0:
+                    for i in range(cols):
+                        if matrix[r][i] != 0:
+                            matrix[r][i] = "*"
+                    for i in range(rows):
+                        if matrix[i][c] != 0:
+                            matrix[i][c] = "*"
+        for r in range((len(matrix))):
+            for c in range(len(matrix[0])):
+                if matrix[r][c] == "*":
+                    matrix[r][c] = 0
 
 
 
