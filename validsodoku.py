15b30383c1136cf42189f2b39da67c341a02278d
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-05-31 13:09:47 -0400

    Update validsodoku.py

--- validsodoku.py
+++ validsodoku.py
@@ -62,4 +62,13 @@ class Solution(object):
                     res += [(r, board[r][c]), (board[r][c], c), (r // 3, c // 3, board[r][c])]
         return len(res) == len(set(res))
                     
+#5/31/24 review:
 
+class Solution(object):
+    def isValidSudoku(self, board):
+        res = []
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != ".":
+                    res += [(r, board[r][c]), (board[r][c], c), (r // 3, c // 3, board[r][c])]
+        return len(res) == len(set(res))
