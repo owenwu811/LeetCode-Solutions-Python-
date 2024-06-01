3bcc14f5faec3b91596c5ae545b671c7ed5a24bf
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-01 16:53:51 -0400

    Update insertinterval.py

--- insertinterval.py
+++ insertinterval.py
@@ -267,7 +267,21 @@ class Solution:
         res.append(newInterval)
         return res
 
+#6/1/24 review:
 
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        for i in range(len(intervals)):
+            if newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            elif newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            else:
+                newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+        res.append(newInterval)
+        return res
 
 
 
