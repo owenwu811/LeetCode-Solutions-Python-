104609b84607408b3d96bae9a3eccdda7fa4f3b2
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-02 15:56:33 -0400

    Update findmedianfromdatastream.py

--- findmedianfromdatastream.py
+++ findmedianfromdatastream.py
@@ -238,3 +238,27 @@ class MedianFinder:
         #If h1 and h2 have the same number of elements, the median is the average of the top element of h1 (the largest number in the smaller half, remember h1 is storing negatives) and the top element of h2 (the actual smallest number in the larger half).
         return ((-1 * self.a[0]) + self.b[0]) / 2
 
+#6/2/24 refresher:
+
+class MedianFinder:
+    def __init__(self):
+        self.small, self.large = [], []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.small, -1 * num)
+        if self.small and self.large and (-1 * self.small[0]) > self.large[0]:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.small) > len(self.large):
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small):
+            val = -1 * heapq.heappop(self.large)
+            heapq.heappush(self.small, val)
+       
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+        return ((-1 * self.small[0]) + self.large[0]) / 2
