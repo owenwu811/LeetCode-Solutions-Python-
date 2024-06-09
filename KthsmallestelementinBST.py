1f08ae1e0589ba354d83564114c9a2f7ca9de4a3
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-08 22:46:39 -0400

    Update KthsmallestelementinBST.py

--- KthsmallestelementinBST.py
+++ KthsmallestelementinBST.py
@@ -624,4 +624,20 @@ class Solution:
                 return cur.val
             cur = cur.right
             
+#6/8/24 review:
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        n = 0
+        cur = root
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            n += 1
+            if n == k:
+                return cur.val
+            cur = cur.right
 
