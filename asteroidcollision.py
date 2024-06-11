fbc8c10abab3218e104b9200015103e4b63ec90d
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-11 12:29:16 -0400

    Update asteroidcollision.py

--- asteroidcollision.py
+++ asteroidcollision.py
@@ -289,3 +289,21 @@ class Solution:
                 stack.append(a) #[5]
         return stack
 
+#6/11/24 review:
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for a in asteroids:
+            while stack and a < 0 and stack[-1] > 0:
+                diff = a + stack[-1]
+                if diff > 0:
+                    a = 0
+                elif diff < 0:
+                    stack.pop()
+                else:
+                    stack.pop()
+                    a = 0
+            if a != 0:
+                stack.append(a)
+        return stack
