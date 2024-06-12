8a6ea78f64688a98ae579137c628de73ed005e73
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-11 23:07:50 -0400

    Create StringToInteger(atoi).py

---
+++ StringToInteger(atoi).py
@@ -0,0 +1,23 @@
+
+
+#Input: s = "42"
+
+#Output: 42
+
+#Input: s = " -042"
+
+#Output: -42
+
+#Input: s = "1337c0d3"
+
+#Output: 1337
+
+#python3 solution:
+
+class Solution:
+    def myAtoi(self, s):
+        match = re.match(r'^\s*([+-]?\d+)', s)
+        if match:
+            integer = int(match.group())
+            return max(-(1 << 31), min(integer, (1 << 31) - 1))
+        return 0
