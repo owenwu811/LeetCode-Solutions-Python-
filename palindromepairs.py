da66f229231ada5f52280ce49fa0937375f55d4f
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-12 22:12:33 -0400

    Update palindromepairs.py

--- palindromepairs.py
+++ palindromepairs.py
@@ -1,4 +1,15 @@
+#You are given a 0-indexed array of unique strings words.
 
+#A palindrome pair is a pair of integers (i, j) such that:
+
+#0 <= i, j < words.length,
+#i != j, and
+#words[i] + words[j] (the concatenation of the two strings) is a
+#palindrome
+#.
+#Return an array of all the palindrome pairs of words.
+
+#You must write an algorithm with O(sum of words[i].length) runtime complexity.
 
 
 
