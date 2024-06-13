b042327f3a853d2cde3b5bc66feaaac37d4f6bca
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-13 10:13:22 -0400

    Update palindromepairs.py

--- palindromepairs.py
+++ palindromepairs.py
@@ -71,4 +71,24 @@ class Solution:
 #When suffix = "tab":
 #Check if "tab" exists in word_dict and its index is not 0.
 #word_dict["tab"] is 1, which is different from 0, so [1, 0] is a valid pair.
-        
+
+
+#6/13/24 review:
+
+class Solution:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        def isp(word):
+            return word == word[::-1]
+        mydict = {word[::-1]: i for i, word in enumerate(words)}
+        res = []
+        for i, word in enumerate(words):
+            n = len(word)
+            for j in range(n + 1):
+                prefix = word[:j]
+                suffix = word[j:]
+                if isp(prefix) and suffix in mydict and mydict[suffix] != i:
+                    res.append([mydict[suffix], i])
+                if j != n and isp(suffix) and prefix in mydict and mydict[prefix] != i:
+                    res.append([i, mydict[prefix]])
+        return res
+
