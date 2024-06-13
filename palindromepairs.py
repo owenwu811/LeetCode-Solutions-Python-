7c588cb08f84f3e464f2aae007c6d01152fa3712
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-12 22:16:11 -0400

    Update palindromepairs.py

--- palindromepairs.py
+++ palindromepairs.py
@@ -12,8 +12,30 @@
 #You must write an algorithm with O(sum of words[i].length) runtime complexity.
 
 
+#my own naive solution that passes 134/136 test cases but fails with "time limit exceeded":
 
-#python3 solution:
+class Solution:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        def ispalindrome(a): 
+            l, r = 0, len(a) - 1
+            while l <= r: 
+                if a[l] != a[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+        res = []
+        for i in range(len(words)):
+            for j in range(i + 1, len(words)):
+                if ispalindrome(words[i] + words[j]):
+                    res.append([i, j])
+                if ispalindrome(words[j] + words[i]):
+                    res.append([j, i])
+        return res
+
+
+
+#correct python3 solution:
 
 from typing import List
 
