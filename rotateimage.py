f3b0a8fc6eda69c71334a1560c21e8e434499b74
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-14 21:31:45 -0400

    Update rotateimage.py

--- rotateimage.py
+++ rotateimage.py
@@ -217,4 +217,17 @@ class Solution:
                 matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
         for i in range(len(matrix)):
             matrix[i].reverse()
-        
+
+#6/14/24 review:
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows, cols = len(matrix), len(matrix[0])
+        for r in range(rows):
+            for c in range(r, cols):
+                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+        for i in range(rows):
+            matrix[i].reverse()
