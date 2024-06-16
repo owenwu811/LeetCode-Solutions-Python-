c9739a52bdf84f67f831bd5f718e5b27fa03c102
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-16 19:05:01 -0400

    Update sortlist.py

--- sortlist.py
+++ sortlist.py
@@ -256,3 +256,38 @@ class Solution:
         elif l2 and not l1:
             cur.next = l2
         return dummy.next
+
+#6/16/24 (missed):
+
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        first, second = head, self.getmid(head) #head is first because we need to sort both halves of the list, not just the second half if you did self.getmid(head) for both!
+        tmp = second.next
+        second.next = None #we don't need first = second after this!
+        second = tmp
+        left, right = self.sortList(first), self.sortList(second)
+        return self.merge(left, right)
+    def getmid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    def merge(self, l1, l2):
+        dummy = ListNode(None)
+        cur = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        if l1 and not l2:
+            cur.next = l1
+        elif l2 and not l1:
+            cur.next = l2
+        return dummy.next
