09be9edd7d022842f59257b9b32c88f37545ddd7
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-16 10:47:28 -0400

    Update palindromepairs.py

--- palindromepairs.py
+++ palindromepairs.py
@@ -92,3 +92,22 @@ class Solution:
                     res.append([i, mydict[prefix]])
         return res
 
+#6/16/24 review:
+
+class Solution:
+    def palindromePairs(self, words: List[str]) -> List[List[int]]:
+        def isp(word):
+            return word == word[::-1]
+        mydict = {word[::-1]: i for i, word in enumerate(words)}
+        res = []
+        for i, word in enumerate(words):
+            n = len(word)
+            for j in range(n + 1):
+                prefix = word[:j]
+                suffix = word[j:]
+                if isp(prefix) and suffix in mydict and mydict[suffix] != i:
+                    res.append([mydict[suffix], i])
+                if j != n and isp(suffix) and prefix in mydict and mydict[prefix] != i:
+                    res.append([i, mydict[prefix]])
+        return res
+                
