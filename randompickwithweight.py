a74406bf78f089b757898cc18ad0ead4ae1d6d39
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-17 19:06:14 -0400

    Update randompickwithweight.py

--- randompickwithweight.py
+++ randompickwithweight.py
@@ -229,4 +229,27 @@ class Solution:
             else:
                 r = mid
         return l
+
+
+#6/17/24 review:
+
+class Solution:
+    def __init__(self, w: List[int]):
+        self.w = w
+        totsum = sum(w)
+        for i in range(len(w)):
+            self.w[i] = self.w[i] / totsum
+        for i in range(1, len(w)):
+            self.w[i] += self.w[i - 1]
+        
+    def pickIndex(self) -> int:
+        randomn = random.uniform(0, 1)
+        l, r = 0, len(self.w) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if randomn > self.w[mid]:
+                l = mid + 1
+            else:
+                r = mid
+        return l
         
