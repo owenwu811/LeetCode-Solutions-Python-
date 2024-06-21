df17508874a3db0e36e713ae07d7ab78a8c878c9
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-21 17:32:42 -0400

    Update lettercombinationsofaphonenumber.py

--- lettercombinationsofaphonenumber.py
+++ lettercombinationsofaphonenumber.py
@@ -368,3 +368,21 @@ class Solution:
         res = []
         f("")
         return res
+
+#6/21/24 review:
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+        res = []
+        mapping = {"2": ['a', 'b', 'c'], "3": ['d', 'e', 'f'], "4": ['g', 'h', 'i'], "5": ['j', 'k', 'l'], "6": ['m', 'n', 'o'], "7": ['p', 'q', 'r', 's'], "8": ['t', 'u', 'v'], "9": ['w', 'x', 'y', 'z']}
+        child = ""
+        def f(i, a):
+            if len(a) == len(digits):
+                res.append(a)
+            else:
+                for d in mapping[digits[len(a)]]:
+                    f(d, a + d)
+        f(0, "")
+        return res
+
