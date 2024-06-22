9dbadf170a888c988b126419492f55f7745c265e
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-22 16:59:30 -0400

    Update NumberofIslands.py

--- NumberofIslands.py
+++ NumberofIslands.py
@@ -439,3 +439,24 @@ class Solution:
                     res += 1
                     dfs(grid, r, c)
         return res
+
+#6/22/24 review:
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(r, c, grid):
+            if r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == "0":
+                return
+            grid[r][c] = "0"
+            dfs(r + 1, c, grid)
+            dfs(r - 1, c, grid)
+            dfs(r, c + 1, grid)
+            dfs(r, c - 1, grid)
+        res = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == "1":
+                    res += 1
+                    dfs(r, c, grid)
+        return res
+
