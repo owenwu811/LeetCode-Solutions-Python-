b8c766836b31002226d8eb95ffdb5dddca3e14b8
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-24 10:10:13 -0400

    Update StringToInteger(atoi).py

--- StringToInteger(atoi).py
+++ StringToInteger(atoi).py
@@ -51,3 +51,14 @@ class Solution:
             integer = int(match.group())
             return max(-(1 << 31), min(integer, (1 << 31) - 1))
         return 0
+
+#6/24/24 refresher:
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        match = re.match(r'^\s*([+-]?\d+)', s)
+        if match:
+            integer = int(match.group())
+            return max(-(1 << 31), min(integer, (1 << 31) - 1))
+        return 0
+        
