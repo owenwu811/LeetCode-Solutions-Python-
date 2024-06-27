5fbf8501198d7fd8acd5a767dde23c44c2ba8f3f
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-27 09:14:21 -0400

    Update sortlist.py

--- sortlist.py
+++ sortlist.py
@@ -291,3 +291,42 @@ class Solution:
         elif l2 and not l1:
             cur.next = l2
         return dummy.next
+
+#6/27/24 review:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        first, second = head, self.getMid(head)
+        tmp = second.next
+        second.next = None
+        left, right = self.sortList(first), self.sortList(tmp)
+        return self.merge(left, right)
+        
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    def merge(self, l1, l2):
+        dummy = cur = ListNode(None)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        if l1 and not l2:
+            cur.next = l1
+        if l2 and not l1:
+            cur.next = l2
+        return dummy.next
