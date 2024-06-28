5b5bd8445a8425f41160b2ece17a34b070b46cae
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-06-28 11:46:02 -0400

    Update combinationsumiv.py

--- combinationsumiv.py
+++ combinationsumiv.py
@@ -185,4 +185,16 @@ class Solution:
                     d[levelamount] += d.get(levelamount - n, 0)
         return d[target]
 
-            
+
+#6/28/24 review:
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        d = {0: 1}
+        for levelamount in range(1, target + 1):
+            d[levelamount] = 0
+            for n in nums:
+                if n <= levelamount:
+                    d[levelamount] += d[levelamount - n]
+        return d[levelamount]
+
