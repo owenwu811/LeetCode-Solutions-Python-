717a15ed9bca93f0c5fd486299e7d7455b5e9f92
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-06 17:07:24 -0400

    Update movezeros.py

--- movezeros.py
+++ movezeros.py
@@ -146,5 +146,16 @@ class Solution:
                 l += 1
             elif nums[l] != 0:
                 l += 1
+
+#7/6/24 review:
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        l = 0
+        for r in range(len(nums)):
+            if nums[l] == 0 and nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+            elif nums[r] != 0:
+                l += 1
             
-        
