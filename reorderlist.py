3f203c78333045c9b0b5acabee44fb2ed1614016
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-09 11:59:11 -0400

    Update reorderlist.py

--- reorderlist.py
+++ reorderlist.py
@@ -389,3 +389,30 @@ second = 4
 
 #final list:
 1 -> 4 -> 2 -> 3
+
+#7/9/24 refresher (missed yesterday):
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        second = slow.next
+        prev = slow.next = None
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+        first, second = head, prev #1 4
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
