6e440038882d417d3e98c31992de71c77074b31c
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-10 14:33:32 -0400

    Update gasstation.py

--- gasstation.py
+++ gasstation.py
@@ -198,3 +198,16 @@ class Solution:
                 starting, res = 0, i + 1
         return res
             
+#7/10/24 refresher:
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost): return -1
+        idx = 0
+        tank = 0
+        for i in range(len(gas)):
+            tank += (gas[i] - cost[i])
+            if tank < 0:
+                tank, idx = 0, i + 1
+        return idx
+            
