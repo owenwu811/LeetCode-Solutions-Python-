fbca94af8776166bc35d36a35b4e5963d9708bbb
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-11 21:32:42 -0400

    Update lettercombinationsofaphonenumber.py

--- lettercombinationsofaphonenumber.py
+++ lettercombinationsofaphonenumber.py
@@ -386,3 +386,21 @@ class Solution:
         f(0, "")
         return res
 
+
+#7/11/24 refresher:
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits: return []
+        mapping = {"2": ['a', 'b', 'c'], "3": ['d', 'e', 'f'], "4": ['g', 'h', 'i'], "5": ['j', 'k', 'l'], "6": ['m', 'n', 'o'], "7": ['p', 'q', 'r', 's'], "8": ['t', 'u', 'v'], "9": ['w', 'x', 'y', 'z']}
+        self.res = []
+        def f(i, word):
+            if len(word) == len(digits):
+                self.res.append(word)
+                return
+            for letter in mapping[digits[len(word)]]: #letter = a, b, c
+                f(i + 1, word + letter)
+        f(0, "")
+        return self.res
+
+
