cd79dcbfa081eed6b83960ae18f135bc7950b3c3
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-15 11:57:10 -0400

    Update findkclosestelements.py

--- findkclosestelements.py
+++ findkclosestelements.py
@@ -175,4 +175,16 @@ class Solution:
             else:
                 r -= 1
         return arr[l: r + 1]
-                
+
+
+#7/15/24 refresher (my own solution):
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - 1
+        while (r - l + 1) > k:
+            if abs(arr[l] - x) <= abs(arr[r] - x):
+                r -= 1
+            else:
+                l += 1
+        return arr[l: r + 1]
