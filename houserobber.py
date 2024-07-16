e6b597df3543e76c572315e69cf56fd4979d1c16
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-15 20:44:32 -0400

    Update houserobber.py

--- houserobber.py
+++ houserobber.py
@@ -156,3 +156,13 @@ class Solution:
             rob2 = tmp
         return rob2
 
+#7/15/24 practice (my own solution):
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+        for n in nums:
+            tmp = max(rob2, n + rob1)
+            rob1 = rob2
+            rob2 = tmp
+        return rob2
