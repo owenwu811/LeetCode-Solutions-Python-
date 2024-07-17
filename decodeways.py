273bee6cea065bd6483453b4aed5861dc892e964
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-17 10:09:39 -0400

    Update decodeways.py

--- decodeways.py
+++ decodeways.py
@@ -180,3 +180,23 @@ class Solution:
             if 10 <= doubledigit <= 26:
                 dp[i] += dp[i - 2]
         return dp[inputlen]
+
+#7/17/24 refresher:
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == "0": return 0
+        inputlen = len(s)
+        dp = [0] * (inputlen + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, len(s) + 1):
+            singledigit = int(s[i - 1: i])
+            if 1 <= singledigit <= 9:
+                dp[i] += dp[i - 1]
+            doubledigit = int(s[i - 2: i])
+            if 10 <= doubledigit <= 26:
+                dp[i] += dp[i - 2]
+
+        return dp[inputlen]
+
