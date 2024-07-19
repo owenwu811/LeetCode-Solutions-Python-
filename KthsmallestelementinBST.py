c9c07ba551b7e69a5f54091e92628aa5c944030a
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-18 21:45:31 -0400

    Update KthsmallestelementinBST.py

--- KthsmallestelementinBST.py
+++ KthsmallestelementinBST.py
@@ -665,3 +665,27 @@ class Solution:
             if n == k:
                 return cur.val
             cur = cur.right
+
+#7/18/24 review:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        cur = root
+        stack = []
+        n = 0
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            n += 1
+            if n == k:
+                return cur.val
+            cur = cur.right
+
