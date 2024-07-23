ea0f7689e2917c602f9430586c2a8688c2595e77
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-22 22:34:29 -0400

    Update largestnumber.py

--- largestnumber.py
+++ largestnumber.py
@@ -192,3 +192,17 @@ class Solution:
                 return -1
         sortedl = sorted(nums, key=cmp_to_key(f))
         return str(int("".join(sortedl)))
+
+#7/22/24 review (missed today):
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i, n in enumerate(nums):
+            nums[i] = str(n)
+        def dfs(a, b):
+            if b + a > a + b:
+                return 1
+            else:
+                return -1
+        sortedl = sorted(nums, key=cmp_to_key(dfs))
+        return str(int("".join(sortedl)))
