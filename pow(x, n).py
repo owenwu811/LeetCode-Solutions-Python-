1bb63b03b5b95bf0b35bf2afa8bd358ad21eb738
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-26 15:20:44 -0400

    Update pow(x, n).py

--- pow(x, n).py
+++ pow(x, n).py
@@ -166,3 +166,20 @@ class Solution:
             x *= x
             n //= 2
         return x * self.myPow(x, n - 1)
+
+#7/26/24 (my solution in python3):
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n *= -1
+        if n % 2 == 0:
+            x *= x
+            n = n / 2
+        return x * self.myPow(x, n - 1)
+
+        #2 ^ -2 = 1 / 4
+
