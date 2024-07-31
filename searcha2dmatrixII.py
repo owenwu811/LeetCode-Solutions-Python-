edb39f8c4345731af1d9419ed913096d4c347086
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-31 11:14:10 -0400

    Update searcha2dmatrixII.py

--- searcha2dmatrixII.py
+++ searcha2dmatrixII.py
@@ -11,15 +11,15 @@ class Solution:
         if not matrix or not matrix[0]:
             return False
         
-        rows, cols = len(matrix), len(matrix[0])
-        row, col = 0, cols - 1
+        rows, cols = len(matrix), len(matrix[0]) #rows, cols = 5, 5
+        row, col = 0, cols - 1 #row, col = 0, 4
         
-        while row < rows and col >= 0:
-            if matrix[row][col] == target:
+        while row < rows and col >= 0: #while 0 < 5 and 4 >= 0, 0 < 5 and 3 >= 0, 0 < 5 and 2 >= 0, 0 < 5 and 1 >= 0, 1 < 5 and 1 >= 0
+            if matrix[row][col] == target: #matrix[0][4] = 15, and 15 != 5 (our target), matrix[0][3] = 11, and 11 != 5 (our target), matrix[0][2] = 7, and 7 != 5, matrix[0][1] = 4, and 4 != 5, matrix[1][1] = 5, and 5 == 5, so we found our target, so return True
                 return True
-            elif matrix[row][col] > target: #we move from 15 to 11 to 7 to 4, and then 4 is not bigger than 5 and is not equal to 5, so 4 is less than 5, so move down to cell (1, 1) in the else block
+            elif matrix[row][col] > target: #since 15 > 5, we move col down one...we move from 15 to 11 to 7 to 4, and then 4 is not bigger than 5 and is not equal to 5, so 4 is less than 5, so move down to cell (1, 1) in the else block
                 col -= 1
-            else: #instead of moving left, we move down
+            else: #instead of moving left, we move down, so we go from matrix[0][1] to matrix[1][1], and we call the while loop again
                 row += 1
         
         return False
