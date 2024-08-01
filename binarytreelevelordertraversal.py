7113b494e41ea9b29d78245878205b6df5dd06b8
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-07-31 23:21:51 -0400

    Update binarytreelevelordertraversal.py

--- binarytreelevelordertraversal.py
+++ binarytreelevelordertraversal.py
@@ -668,3 +668,30 @@ class Solution:
             if level:
                 res.append(level)
         return res
+
+#7/31/24 refresher:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        d = deque()
+        d.append(root)
+        res = []
+        while d:
+            level = []
+            for i in range(len(d)):
+                cur = d.popleft()
+                if cur:
+                    level.append(cur.val)
+                    d.append(cur.left)
+                    d.append(cur.right)
+            if level:
+                res.append(level)
+        return res
+
+
