9ccae73748704ed78461e0e32f5b5d2d8634ac90
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-08-04 16:09:59 -0400

    Update wordbreak.py

--- wordbreak.py
+++ wordbreak.py
@@ -318,4 +318,14 @@ class Solution:
                     res[j + 1] = res[i] or res[j + 1]
         return res[-1]
         
+#8/4/24 refresher:
 
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        res = [True] + ([False] * len(s))
+        for i in range(len(s)):
+            for j in range(i, len(s)):
+                if s[i: j + 1] in wordDict:
+                    res[j + 1] = res[i] or res[j + 1]
+        return res[-1]
+        
