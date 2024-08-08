ea8809ba38e9676a1d7827d63a3888f3606cbc4a
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-08-08 09:29:15 -0400

    Update addtwonumbers.py

--- addtwonumbers.py
+++ addtwonumbers.py
@@ -194,4 +194,29 @@ val = 10 % 10 = 0 #val = val % 10
 #wrong because carry depends on the value that just changed to 0
 #right because carry depends on the value that stayed as 10
 
+#8/8/24 refresher:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        carry = 0
+        dummy = ListNode(0)
+        current = dummy
+        while l1 or l2 or carry > 0:
+            l1val = l1.val if l1 else 0
+            l2val = l2.val if l2 else 0
+            val = l1val + l2val + carry
+            carry = val // 10
+            val = val % 10
+            current.next = ListNode(val)
+            current = current.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return dummy.next
+            
+
 
