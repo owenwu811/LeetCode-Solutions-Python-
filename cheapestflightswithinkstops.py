63880837a2d769b8b6e1a6def8a70baf1d1bb7c4
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-08-21 16:47:18 -0400

    Update cheapestflightswithinkstops.py

--- cheapestflightswithinkstops.py
+++ cheapestflightswithinkstops.py
@@ -193,4 +193,20 @@ class Solution:
                     tmpprices[d] = prices[s] + p
             prices = tmpprices
         return prices[dst] if prices[dst] != float('inf') else -1
- 
+
+#8/21/24 review:
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float('inf')] * n
+        prices[src] = 0
+        for i in range(k + 1):
+            tmp = prices.copy()
+            for s, d, p in flights:
+                if prices[s] == float('inf'):
+                    continue
+                if prices[s] + p < tmp[d]:
+                    tmp[d] = prices[s] + p
+            prices = tmp
+        return prices[dst] if prices[dst] != float('inf') else -1
+        
