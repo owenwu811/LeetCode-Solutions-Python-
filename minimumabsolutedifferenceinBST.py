899d0fe4eb68024231e7c3a12cc1c7930b995684
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-08-21 00:50:35 -0400

    Update minimumabsolutedifferenceinBST.py

--- minimumabsolutedifferenceinBST.py
+++ minimumabsolutedifferenceinBST.py
@@ -1,5 +1,6 @@
 #easy
 #58.5%acceptancerate
+#530
 
 
 #Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
