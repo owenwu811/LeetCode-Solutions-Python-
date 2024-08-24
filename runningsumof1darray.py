1c2b31e028b809abc0336598bf0a7e121a88b86d
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-08-24 13:56:52 -0400

    Create runningsumof1darray.py

---
+++ runningsumof1darray.py
@@ -0,0 +1,25 @@
+#Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+#Return the running sum of nums.
+
+ 
+
+#Example 1:
+
+#Input: nums = [1,2,3,4]
+#Output: [1,3,6,10]
+#Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+
+
+#my own solution using python3:
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        totsum = sum(nums)
+        res = [0] * len(nums)
+        cursum = 0
+        for i in range(len(nums)):
+            cursum += nums[i]
+            res[i] = cursum
+        return res
