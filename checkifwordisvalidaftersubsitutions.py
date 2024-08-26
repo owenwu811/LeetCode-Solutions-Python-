a85b55aa3cb091a0bc0f50f2464875c2b80eaa16
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-08-26 18:01:48 -0400

    Create checkifwordisvalidaftersubsitutions.py

---
+++ checkifwordisvalidaftersubsitutions.py
@@ -0,0 +1,18 @@
+
+#Given a string s, determine if it is valid.
+
+#A string s is valid if, starting with an empty string t = "", you can transform t into s after performing the following operation any number of times:
+
+#Insert string "abc" into any position in t. More formally, t becomes tleft + "abc" + tright, where t == tleft + tright. Note that tleft and tright may be empty.
+#Return true if s is a valid string, otherwise, return false.
+
+
+#clever python3 solution (couldn't solve by myself):
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        tmp = ""
+        while tmp != s:
+            tmp = s
+            s = s.replace("abc", "")
+        return s == "" #if s is what tmp started at, then it's possible 
