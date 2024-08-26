b83ace077d09b99d480500675cc6ef0840edeccb
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-08-26 17:42:45 -0400

    Create findthenthvalueafterkseconds.py

---
+++ findthenthvalueafterkseconds.py
@@ -0,0 +1,28 @@
+#medium
+#54.6% acceptance rate
+#3179
+
+#You are given two integers n and k.
+
+#Initially, you start with an array a of n integers where a[i] = 1 for all 0 <= i <= n - 1. After each second, you simultaneously update each element to be the sum of all its preceding elements plus the element itself. For example, after one second, a[0] remains the same, a[1] becomes a[0] + a[1], a[2] becomes a[0] + a[1] + a[2], and so on.
+
+#Return the value of a[n - 1] after k seconds.
+
+#Since the answer may be very large, return it modulo 109 + 7.
+
+
+
+#my own solution using python3 after a small adjustement:
+
+class Solution:
+    def valueAfterKSeconds(self, n: int, k: int) -> int:
+        mod = (10 ** 9) + 7
+        initial = [1] * n
+        print(initial)
+        res = []
+        while k > 0:
+            for i in range(1, len(initial)):
+                initial[i] += initial[i - 1]
+            initial[0] = 1
+            k -= 1
+        return initial[-1] % mod #need to read the instructions that we need to divide by mod!
