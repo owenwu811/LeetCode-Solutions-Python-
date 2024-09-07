5c6ba1b7da449b79bc9be0c97e3e4290691366e0
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-09-06 21:15:16 -0700

    Update foursum.py

--- foursum.py
+++ foursum.py
@@ -234,3 +234,31 @@ class Solution:
                             fourth -= 1
         return res
 
+#9/6/24 review from grokking course (missed again):
+
+def four_sum(nums, target):
+    res = []
+    nums.sort()
+    for first in range(len(nums) - 3):
+      if first > 0 and nums[first] == nums[first - 1]:
+        continue
+      for second in range(first + 1, len(nums) - 2): #DO NOT FORGET TO PLACE THE CORRECT POSITION IN THE RANGE FUNCTINO - RANGE(FIRST + 1)!!!!!
+        if second > first + 1 and nums[second] == nums[second - 1]:
+          continue
+        third = second + 1
+        fourth = len(nums) - 1
+        while third < fourth:
+          foursum = nums[first] + nums[second] + nums[third] + nums[fourth]
+          if foursum < target:
+            third += 1
+          elif foursum > target:
+            fourth -= 1
+          else:
+            res.append([nums[first], nums[second], nums[third], nums[fourth]])
+            third += 1
+            fourth -= 1
+            while third < fourth and nums[third] == nums[third - 1]:
+              third += 1
+            while third < fourth and nums[fourth] == nums[fourth + 1]:
+              fourth -= 1
+    return res
