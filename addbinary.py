f4e67c373e9ce7ead66749ed45d62f88321bab27
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-09-07 21:42:32 -0700

    Update addbinary.py

--- addbinary.py
+++ addbinary.py
@@ -127,3 +127,10 @@ class Solution:
     def addBinary(self, a: str, b: str) -> str:
         res = bin(int(a, 2) + int(b, 2))
         return res[2:]
+
+#9/8/24 review:
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        s = bin(int(a, 2) + int(b, 2))
+        return s[2:]
