eefe8a24194df21f76f0505e8ddd668a71067a2e
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-09-10 19:13:49 -0700

    Update diameterofbinarytree.py

--- diameterofbinarytree.py
+++ diameterofbinarytree.py
@@ -355,4 +355,29 @@ class Solution:
 
         f(root)
         return self.res
+
+
+#9/10/24 review:
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.res = 0
+        def f(root):
+            if not root:
+                return 0
+            l = f(root.left)
+            r = f(root.right)
+            self.res = max(self.res, r + l)
+            return 1 + max(l, r)
+
+
+
+        f(root)
+        return self.res
         
