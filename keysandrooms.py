f641c87e72ffca5e29a901945e2f1e3a525008d5
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-09-10 23:36:43 -0400

    Update keysandrooms.py

--- keysandrooms.py
+++ keysandrooms.py
@@ -12,18 +12,21 @@
 
 #correct python3 solution:
 
+
+#rooms = [[1],[2],[3],[]]
+
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         n = len(rooms)
         visited = [False] * n
-        visited[0] = True
+        visited[0] = True #[True, False, False, False]
         canvisit = 1
         stack = [0] #we have to start by visiting the room at index 0 in our sublist of lists [[1],[2],[3],[]], so [1]
         while stack:
-            current = stack.pop()
+            current = stack.pop() #starts as 0 because stack starts at [0] because we must start by visiting the 0th index room in our input list of lists first
             for key in rooms[current]: #rooms[0] = [1], so key = 1
-                if not visited[key]: #if the room has not been visited yet aka visited[1] is False
-                    stack.append(key)
-                    visited[key] = True #we have to mark it as visited
-                    canvisit += 1
+                if not visited[key]: #if the room has not been visited yet aka visited[1] is False, which it is from [True, False, False, False] initially
+                    stack.append(key) #we popped 0 off the stack, so stack is now [], but since index 1 in visited is False, we append 1 to our stack [1]
+                    visited[key] = True #we have to mark room 1 as visited - [True, True, False, False]
+                    canvisit += 1 #we have been able to visit another room
         return canvisit == n
