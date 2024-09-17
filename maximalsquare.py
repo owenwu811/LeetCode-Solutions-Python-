e690733ea881b2c3fc58a7f72b18d0c03fc6e9e8
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-09-16 23:22:14 -0400

    Update maximalsquare.py

--- maximalsquare.py
+++ maximalsquare.py
@@ -278,3 +278,22 @@ class Solution:
                         dp[r][c] = 1 + min(dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1])
                     sidelength = max(sidelength, dp[r][c])
         return sidelength * sidelength
+
+
+#9/16/24 review:
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        sidelength = 0
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * n for i in range(m)]
+        for r in range(m):
+            for c in range(n):
+                if matrix[r][c] == "1":
+                    if r == 0 or c == 0:
+                        dp[r][c] = 1
+                    else:
+                        dp[r][c] = min(dp[r - 1][c], dp[r][c - 1], dp[r - 1][c - 1]) + 1
+                    sidelength = max(sidelength, dp[r][c])
+        return sidelength * sidelength
+                
