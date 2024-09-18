e4cbd12129b5cf3691a0c88dbbd5d66f70c40a91
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-09-17 22:36:34 -0400

    Update minimumdeletionstomakecharfrequenciesunique.py

--- minimumdeletionstomakecharfrequenciesunique.py
+++ minimumdeletionstomakecharfrequenciesunique.py
@@ -19,9 +19,18 @@ class Solution:
         d = Counter(s)
         res = 0
         seen = set()
-        for i, n in d.items():
+        for i, n in d.items(): #iterating through each key, value pair in the d dictionary using the .items() method!
+            #i = the key while n = the value 
             while n > 0 and n in seen:
                 res += 1
-                n -= 1
-            seen.add(n)
+                n -= 1 #changes the value in d.items(), which is n
+            seen.add(n) #we add n to seen because if we hit 0 as frequency, the character is now not seen before is the idea 
         return res
+
+
+#why are we decrementing until we hit 0?
+
+#Why does this ensure the minimum number of deletions?
+#Decrementing by 1 at each step is the smallest possible change you can make to reduce the frequency. By making the minimal reduction needed to achieve a unique frequency, you ensure that you delete the fewest number of characters.
+#If you try to make bigger jumps (e.g., directly jumping to 0 or skipping frequencies), you would likely end up deleting more characters than necessary.- the idea in this problem is to delete the fewest number of characters to make s good!
+#Once a frequency is reduced to 0, it means the character has been completely removed, which is the worst-case scenario but necessary if no smaller unique frequency is available.
