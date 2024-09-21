52f6c1355ab3c8cb0e649089a6822abdd221a4d3
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-09-21 00:05:59 -0400

    Update globalandlocalinversions.py

--- globalandlocalinversions.py
+++ globalandlocalinversions.py
@@ -55,3 +55,12 @@ class Solution:
             if abs(n - i) > 1:
                 return False
         return True
+
+#my way:
+
+class Solution:
+    def isIdealPermutation(self, nums: List[int]) -> bool:
+        for i in range(len(nums)):
+            if abs(nums[i] - i) > 1:
+                return False
+        return True
