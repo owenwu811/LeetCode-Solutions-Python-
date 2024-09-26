f5573979999615a109de9833d53928168e53e1c2
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-09-25 21:57:46 -0400

    Update BinaryTreePruning.py

--- BinaryTreePruning.py
+++ BinaryTreePruning.py
@@ -18,9 +18,23 @@ class Solution:
         def f(root):
             if not root:
                 return 
-            root.left = f(root.left)
+            root.left = f(root.left) 
             root.right = f(root.right)
+            #we will get here when we hit the bottom left of the tree and determined that we are a leaf on both sides
             if root.val == 0 and not root.left and not root.right:
-                return None
+                return None #this is the actual deletion of the node
             return root
         return f(root)
+
+
+#you can think of it as starting from the bottom up since we dig all the way to the left and prune any nodes with value of 0
+
+
+#         1
+#        / \
+#       0   1
+#      /   / \
+#     0   0   1
+#    N N - this is when we first get to the if root.val == 0 and not root.left and not root.right because we finished with the N to the right of the bottom subtree
+
+# after return None in the 2nd if statement, we backtrack to root.right = f(root.right), which happens to be None since the 2nd level's 0 has a right child of None, so the if root.val == 0 is visited again
