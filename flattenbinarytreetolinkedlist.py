8f79fbd4b95155a09bfc7b3216a1ca33ce2b5ba7
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-09-26 20:25:27 -0400

    Update flattenbinarytreetolinkedlist.py

--- flattenbinarytreetolinkedlist.py
+++ flattenbinarytreetolinkedlist.py
@@ -35,6 +35,65 @@ class Solution:
                         d.appendleft(cur.right)
                     if cur.left:
                         d.appendleft(cur.left) #so because the left subtree is smaller, now the smaller one goes in front for us to set the right pointer to later
-                    cur.left = None
+                    cur.left = None # as neetcode said, we always want to set the left child to None in the picture below:
                     cur.right = d[0]
         return root
+
+
+#picture below:
+
+                        # notice below here how the left is set to N, but the right is always connected to the next number as neetcode mentioned, so this explaines the two lines after the two appendlefts above:
+
+    #     1              #  1
+    #    / \             #    2
+    #   2   5            #      3
+    #  / \   \           #        4
+    # 3   4   6          #          5
+                         #            6
+
+
+#original op's solution:
+
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        q = deque()
+        q.append(root)
+        q.append(None)
+        def print_stack():
+            print("Current deque state: ", [node.val if node else None for node in q])
+        
+        print_stack()
+        print(q)
+        while q[0] is not None:
+            node = q.popleft() 
+            if node.right:
+                q.appendleft(node.right) 
+            if node.left:
+                q.appendleft(node.left) 
+            node.left = None 
+            node.right = q[0] 
+            print_stack()
+        return root
+
+#original op's solution put into pythontutor:
+    
+    # Build the binary tree:
+    #     1
+    #    / \
+    #   2   5
+    #  / \   \
+    # 3   4   6
+
+#Original tree (in level order):
+#[1, 2, 5, 3, 4, None, 6]
+#Current deque state:  [1, None]
+#deque([<__main__.TreeNode object at 0x7f24078b30d0>, None])
+#Current deque state:  [2, 5, None]
+#Current deque state:  [3, 4, 5, None]
+#Current deque state:  [4, 5, None]
+#Current deque state:  [5, None]
+#Current deque state:  [6, None]
+#Current deque state:  [None]
+
+#Flattened tree (as linked list):
+#1 -> 2 -> 3 -> 4 -> 5 -> 6
