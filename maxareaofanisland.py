da6e3414f81c4647e70627a5057f8b5c663645ee
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-02 21:32:54 -0400

    Update maxareaofanisland.py

--- maxareaofanisland.py
+++ maxareaofanisland.py
@@ -30,3 +30,28 @@ class Solution:
                 if grid[r][c] == 1:
                     res = max(res, dfs(r, c))
         return res
+
+
+#10/2/24 review:
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def dfs(r, c):
+            if (r < 0 or r >= len(grid) or c < 0 or c >= len(grid[0]) or grid[r][c] == 0):
+                return 0
+            grid[r][c] = 0
+            result = 1
+            result += dfs(r + 1, c)
+            result += dfs(r - 1, c)
+            result += dfs(r, c + 1)
+            result += dfs(r, c - 1)
+            return result
+
+
+
+        res = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    res = max(res, dfs(r, c))
+        return res
