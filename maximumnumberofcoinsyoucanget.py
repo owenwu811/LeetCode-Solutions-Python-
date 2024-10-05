c19c7f65889147d6dac63cc646b9881564132442
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-05 17:49:48 -0400

    Create maximumnumberofcoinsyoucanget.py

---
+++ maximumnumberofcoinsyoucanget.py
@@ -0,0 +1,28 @@
+
+#1561
+#medium
+
+#There are 3n piles of coins of varying size, you and your friends will take piles of coins as follows:
+
+#In each step, you will choose any 3 piles of coins (not necessarily consecutive).
+#Of your choice, Alice will pick the pile with the maximum number of coins.
+#You will pick the next pile with the maximum number of coins.
+#Your friend Bob will pick the last pile.
+#Repeat until there are no more piles of coins.
+#Given an array of integers piles where piles[i] is the number of coins in the ith pile.
+
+#Return the maximum number of coins that you can have.
+
+
+#my own solution using python3 after looking at another solution:
+
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort(reverse=True)
+        print(piles)
+        res = 0
+        for i in range(len(piles) // 3):
+            piles.pop(0)
+            res += piles.pop(0)
+            piles.pop()
+        return res
