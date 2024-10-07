56c080156dc5d0000699ba7096b3e8372e0db641
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-06 20:59:11 -0400

    Create poweroftwo.py

---
+++ poweroftwo.py
@@ -0,0 +1,29 @@
+
+#231
+#easy
+
+
+#Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+#An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+
+
+#Example 1:
+
+#Input: n = 1
+#Output: true
+#Explanation: 20 = 1
+
+
+#correct python3 solution:
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 0:
+            return False
+        if n == 1:
+            return True
+        if n % 2 == 0:
+            return self.isPowerOfTwo(n // 2)
+        return False
