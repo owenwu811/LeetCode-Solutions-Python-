5d6029cc61caf7171f9085c4f94b564236cd9d73
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-11 14:38:07 -0400

    Create maximumnumberofgroupsenteringacompetition.py

---
+++ maximumnumberofgroupsenteringacompetition.py
@@ -0,0 +1,44 @@
+
+
+
+#2358
+#medium
+
+#You are given a positive integer array grades which represents the grades of students in a university. You would like to enter all these students into a competition in ordered non-empty groups, such that the ordering meets the following conditions:
+
+#The sum of the grades of students in the ith group is less than the sum of the grades of students in the (i + 1)th group, for all groups (except the last).
+#The total number of students in the ith group is less than the total number of students in the (i + 1)th group, for all groups (except the last).
+#Return the maximum number of groups that can be formed.
+
+ 
+
+#Example 1:
+
+#Input: grades = [10,6,12,7,3,5]
+#Output: 3
+#Explanation: The following is a possible way to form 3 groups of students:
+#- 1st group has the students with grades = [12]. Sum of grades: 12. Student count: 1
+#- 2nd group has the students with grades = [6,7]. Sum of grades: 6 + 7 = 13. Student count: 2
+#- 3rd group has the students with grades = [10,3,5]. Sum of grades: 10 + 3 + 5 = 18. Student count: 3
+#It can be shown that it is not possible to form more than 3 groups.
+
+
+#my own solution using python3:
+
+class Solution:
+    def maximumGroups(self, grades: List[int]) -> int:
+        grades.sort()
+        orig = 1
+        actual = []
+        tmp = []
+        while orig <= len(grades):
+            tmp = []
+            j = orig
+            while j > 0:
+                tmp.append(grades.pop())
+                j -= 1
+            actual.append(tmp)
+            orig += 1
+        return len(actual)
+        print(actual)
+            
