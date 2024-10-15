568db4bfe95d269e764829ae1f0eaaec8077686a
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-15 16:14:05 -0400

    Create meetingroomsII.py

---
+++ meetingroomsII.py
@@ -0,0 +1,46 @@
+
+
+#253
+#medium
+
+
+#Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+ 
+
+#Example 1:
+
+#Input: intervals = [[0,30],[5,10],[15,20]]
+#Output: 2
+#Example 2:
+
+#Input: intervals = [[7,10],[2,4]]
+#Output: 1
+
+
+#my own solution using python3 after watching neetcode's explanation:
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort() 
+        start, end = [], []
+        for a, b in intervals:
+            start.append(a)
+            end.append(b)
+        start.sort()
+        end.sort()
+        i, j = 0, 0
+        res = 1
+        cur = 0
+        print(start, end)
+        while i < len(start) and j < len(end):
+            while i < len(start) and j < len(end) and start[i] < end[j]:
+                print(i, j)
+                cur += 1
+                res = max(res, cur)
+                i += 1
+            print(res)
+            cur -= 1 #must decrement cur here because we finished up with that entire pair?
+            j += 1
+        return res
+            
