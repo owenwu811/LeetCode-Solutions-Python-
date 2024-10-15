bdd9d3fd2701feddd0456b6a16577edd4f0f68b7
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-15 12:59:29 -0400

    Update merge intervals.py

--- merge intervals.py
+++ merge intervals.py
@@ -241,4 +241,17 @@ class Solution:
             else:
                 res.append([start, end])
         return res
-        
+
+#10/15/24 review:
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        res = [intervals[0]]
+        for start, end in intervals[1:]:
+            if start <= res[-1][1]:
+                res[-1][1] = max(end, res[-1][1])
+            else:
+                res.append([start, end])
+        return res
+
