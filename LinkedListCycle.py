7e1dcaf4fad735d8aac0d6e31cfb498b428a0f1b
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-18 15:38:49 -0400

    Update and rename LinkedListCycle to LinkedListCycle.py

--- LinkedListCycle.py
+++ LinkedListCycle.py
@@ -1,29 +1,29 @@
-Given head, the head of a linked list, determine if the linked list has a cycle in it.
+#Given head, the head of a linked list, determine if the linked list has a cycle in it.
 
-There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+#There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 
-Return true if there is a cycle in the linked list. Otherwise, return false.
+#Return true if there is a cycle in the linked list. Otherwise, return false.
 
  
 
-Example 1:
+#Example 1:
 
 
-Input: head = [3,2,0,-4], pos = 1
-Output: true
-Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
-Example 2:
+#Input: head = [3,2,0,-4], pos = 1
+#Output: true
+#Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+#Example 2:
 
 
-Input: head = [1,2], pos = 0
-Output: true
-Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
-Example 3:
+#Input: head = [1,2], pos = 0
+#Output: true
+#Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+#Example 3:
 
 
-Input: head = [1], pos = -1
-Output: false
-Explanation: There is no cycle in the linked list.
+#Input: head = [1], pos = -1
+#Output: false
+#Explanation: There is no cycle in the linked list.
 
 
 My Solution:
@@ -193,5 +193,23 @@ class Solution:
             slow = slow.next
             fast = fast.next.next
             if slow == fast: #means they lapped each other aka cycle where no gaps if we use both pointers through entire rink
+                return True
+        return False
+
+#10/18/24 review:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
                 return True
         return False
