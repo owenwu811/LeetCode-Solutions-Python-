892bb97919079fdb05190476a7a1d2f63fe832aa
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-18 19:22:21 -0400

    Update and rename ReverseLinkedList to ReverseLinkedList.py

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -1,21 +1,21 @@
-Given the head of a singly linked list, reverse the list, and return the reversed list.
+#Given the head of a singly linked list, reverse the list, and return the reversed list.
 
  
 
-Example 1:
+#Example 1:
 
 
-Input: head = [1,2,3,4,5]
-Output: [5,4,3,2,1]
-Example 2:
+#Input: head = [1,2,3,4,5]
+#Output: [5,4,3,2,1]
+#Example 2:
 
 
-Input: head = [1,2]
-Output: [2,1]
-Example 3:
+#Input: head = [1,2]
+#Output: [2,1]
+#Example 3:
 
-Input: head = []
-Output: []
+#Input: head = []
+#Output: []
 
 #this is a variation of two pointers 
 
@@ -106,3 +106,25 @@ class Solution:
             prev = cur
             cur = nxt
         return prev
+
+
+#10/18/24 (my own solution using python3):
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        tmp = []
+        while head:
+            tmp.append(head.val)
+            head = head.next
+        tmp = tmp[::-1]
+        dummy = ListNode(None)
+        cur = dummy
+        for t in tmp:
+            cur.next = ListNode(t)
+            cur = cur.next
+        return dummy.next
