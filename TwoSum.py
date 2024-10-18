375f0f78a390472631b77042c24842fc7e8d8389
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-18 00:34:23 -0400

    Update and rename TwoSum to TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,26 +1,26 @@
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
-You can return the answer in any order.
+#You can return the answer in any order.
 
  
 
-Example 1:
+#Example 1:
 
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-Example 2:
+#Input: nums = [2,7,11,15], target = 9
+#Output: [0,1]
+#Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+#Example 2:
 
-Input: nums = [3,2,4], target = 6
-Output: [1,2]
-Example 3:
+#Input: nums = [3,2,4], target = 6
+#Output: [1,2]
+#Example 3:
 
-Input: nums = [3,3], target = 6
-Output: [0,1]
+#Input: nums = [3,3], target = 6
+#Output: [0,1]
 
-My Solution:
+#My Solution:
 
 #flipping trick is applied here with reverse dictionary 
 
@@ -95,3 +95,14 @@ class Solution:
             if complement in mydict:
                 return mydict[complement], i
             mydict[n] = i
+
+
+#my own solution using python3 (brute force way on 10/18/24):
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+        
