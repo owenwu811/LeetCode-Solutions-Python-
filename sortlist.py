70f42ed5507885e1f86f4a9db538951270b73fda
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-19 11:19:01 -0400

    Update sortlist.py

--- sortlist.py
+++ sortlist.py
@@ -406,5 +406,24 @@ class Solution:
             cur.next = l2
         return dummy.next
 
+#my own solution on 10/19/24 using python3:
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        tmp = []
+        while head:
+            tmp.append(head.val)
+            head = head.next
+        tmp.sort()
+        dummy = ListNode(None)
+        cur = dummy
+        for t in tmp:
+            cur.next = ListNode(t)
+            cur = cur.next
+        return dummy.next
         
