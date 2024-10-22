be03811e3a0a770231747d08b49c6f597aa8bebd
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-22 15:49:42 -0400

    Update MergeTwoSortedLists.py

--- MergeTwoSortedLists.py
+++ MergeTwoSortedLists.py
@@ -372,3 +372,36 @@ class Solution:
             cur.next = list2
         return dummy.next
 
+
+#my own solution using python3 on 10/22/24:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        one, two = [], []
+        while list1:
+            one.append(list1.val)
+            list1 = list1.next
+        while list2:
+            two.append(list2.val)
+            list2 = list2.next
+        print(one)
+        print(two)
+        new = []
+        for i in range(len(one)):
+            print(one[i])
+            new.append(one[i])
+        for i in range(len(two)):
+            new.append(two[i])
+        print(new)
+        new.sort()
+        dummy = ListNode(None)
+        cur = dummy
+        for n in new:
+            cur.next = ListNode(n)
+            cur = cur.next
+        return dummy.next
