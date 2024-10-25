44a46ea84ab955e984688d0bb8a0caaa5be51d89
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-10-25 19:26:19 -0400

    Update and rename ValidAnagram to ValidAnagram.py

--- ValidAnagram.py
+++ ValidAnagram.py
@@ -1,19 +1,19 @@
-Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+#Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 
-An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+#An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
  
 
-Example 1:
+#Example 1:
 
-Input: s = "anagram", t = "nagaram"
-Output: true
-Example 2:
+#Input: s = "anagram", t = "nagaram"
+#Output: true
+#Example 2:
 
-Input: s = "rat", t = "car"
-Output: false
+#Input: s = "rat", t = "car"
+#Output: false
 
-My Solution:
+#My Solution:
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
@@ -175,3 +175,10 @@ class Solution:
                 return False
             t = t.replace(char, "", 1)
         return True
+
+
+#my own solution using python3 on 10/25/24:
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
