3fea2e3c4bad4ba03b2300031f3949889b2fbf76
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-12-11 22:44:55 -0500

    Create allpathsfromsourcetotarget.py

---
+++ allpathsfromsourcetotarget.py
@@ -0,0 +1,24 @@
+#797
+#medium
+
+
+#Given a directed acyclic graph (DAG) of n nodes labeled from 0 to n - 1, find all possible paths from node 0 to node n - 1 and return them in any order.
+
+#The graph is given as follows: graph[i] is a list of all nodes you can visit from node i (i.e., there is a directed edge from node i to node graph[i][j]).
+
+
+#correct python3 solution (could not solve myself):
+
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        self.res = []
+        def dfs(idx, cur):
+            if idx == len(graph) - 1:
+                self.res.append(cur.copy())
+                return 
+            for nei in graph[idx]:
+                cur.append(nei)
+                dfs(nei, cur)
+                cur.pop()
+        dfs(0, [0])
+        return self.res
