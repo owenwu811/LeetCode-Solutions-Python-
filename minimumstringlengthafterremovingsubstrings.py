1938fb231cfb4f9058bf1995c75a60ce40f09384
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-12-16 15:55:24 -0500

    Create minimumstringlengthafterremovingsubstrings.py

---
+++ minimumstringlengthafterremovingsubstrings.py
@@ -0,0 +1,34 @@
+#2696
+#easy
+
+#You are given a string s consisting only of uppercase English letters.
+
+#You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+
+#Return the minimum possible length of the resulting string that you can obtain.
+
+#Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
+
+ 
+
+#Example 1:
+
+#Input: s = "ABFCACDB"
+#Output: 2
+#Explanation: We can do the following operations:
+#- Remove the substring "ABFCACDB", so s = "FCACDB".
+#- Remove the substring "FCACDB", so s = "FCAB".
+#- Remove the substring "FCAB", so s = "FC".
+#So the resulting length of the string is 2.
+#It can be shown that it is the minimum length that we can obtain.
+
+
+#my own solution using python3:
+
+class Solution:
+    def minLength(self, s: str) -> int:
+        while 'AB' in s or 'CD' in s:
+            s = s.replace("AB", "")
+            s = s.replace("CD", "")
+            print(s)
+        return len(s)
