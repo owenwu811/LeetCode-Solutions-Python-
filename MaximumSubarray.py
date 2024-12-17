5d330ca656c0b0d746bf041c5890139e96dd902e
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-12-16 21:58:28 -0500

    Update and rename Maximum Subarray to MaximumSubarray.py

--- MaximumSubarray.py
+++ MaximumSubarray.py
@@ -1,29 +1,41 @@
-Given an integer array nums, find the 
-subarray with the largest sum, and return its sum.
+#Given an integer array nums, find the 
+#subarray with the largest sum, and return its sum.
 
-Example 1:
+#Example 1:
 
-Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
-Output: 6
-Explanation: The subarray [4,-1,2,1] has the largest sum 6.
-Example 2:
+#Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+#Output: 6
+#Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+#Example 2:
 
-Input: nums = [1]
-Output: 1
-Explanation: The subarray [1] has the largest sum 1.
-Example 3:
+#Input: nums = [1]
+#Output: 1
+#Explanation: The subarray [1] has the largest sum 1.
+#Example 3:
 
-Input: nums = [5,4,-1,7,8]
-Output: 23
-Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+#Input: nums = [5,4,-1,7,8]
+#Output: 23
+#Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
  
 
-Constraints:
+#Constraints:
 
-1 <= nums.length <= 105
--104 <= nums[i] <= 104
+#1 <= nums.length <= 105
+#-104 <= nums[i] <= 104
 
-My Solution:
+
+#dumb way (will get TLE):
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = min(nums)
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                subarr = nums[i: j + 1]
+                res = max(res, sum(subarr))
+        return res
+
+#My Solution:
 
 class Solution:
     import math
@@ -43,7 +55,7 @@ class Solution:
   #line 38 is necessary
 
 
-You could also do it like this: 
+#You could also do it like this: 
 
 class Solution:
     import math
@@ -269,4 +281,17 @@ class Solution:
                 subarraysum = 0
             subarraysum += n
             res = max(res, subarraysum)
+        return res
+
+#12/16/24 review:
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = min(nums)
+        subarr = 0
+        for n in nums:
+            if subarr < 0:
+                subarr = 0
+            subarr += n
+            res = max(res, subarr)
         return res
