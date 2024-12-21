db3d8abefc12ba15d0ed8d98cb3c61c2ade4351d
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-12-21 15:12:06 -0500

    Update longestcommonsubsequence.py

--- longestcommonsubsequence.py
+++ longestcommonsubsequence.py
@@ -28,3 +28,24 @@ class Solution:
                 else:
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
         return dp[-1][-1]
+
+
+#12/21/24 review:
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = []
+        m, n = len(text1), len(text2)
+        for i in range(m + 1):
+            cur = []
+            for j in range(n + 1):
+                cur.append(0)
+            dp.append(cur)
+        print(dp)
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[-1][-1]
