eb0139808e91049706ddd266a2b1b2ceeceec2d8
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2024-12-27 13:19:23 -0500

    Create countsymmetricintegers.py

---
+++ countsymmetricintegers.py
@@ -0,0 +1,46 @@
+#2843
+#easy
+
+#You are given two positive integers low and high.
+
+#An integer x consisting of 2 * n digits is symmetric if the sum of the first n digits of x is equal to the sum of the last n digits of x. Numbers with an odd number of digits are never symmetric.
+
+#Return the number of symmetric integers in the range [low, high].
+
+ 
+
+#Example 1:
+
+#Input: low = 1, high = 100
+#Output: 9
+#Explanation: There are 9 symmetric integers between 1 and 100: 11, 22, 33, 44, 55, 66, 77, 88, and 99.
+#Example 2:
+
+#Input: low = 1200, high = 1230
+#Output: 4
+#Explanation: There are 4 symmetric integers between 1200 and 1230: 1203, 1212, 1221, and 1230.
+
+
+#my own solution using python3:
+
+class Solution:
+    def countSymmetricIntegers(self, low: int, high: int) -> int:
+        res = 0
+        for i in range(low, high + 1):
+            cur = str(i)
+            if len(cur) % 2 != 0:
+                continue
+            half = len(cur) // 2
+            firsthalf = cur[:half]
+            secondhalf = cur[half:]
+            print(cur, firsthalf, secondhalf)
+            firsthsum, secondhalfs = 0, 0
+            for f in firsthalf:
+                firsthsum += int(f)
+            for s in secondhalf:
+                secondhalfs += int(s)
+            if firsthsum == secondhalfs:
+                res += 1
+            
+
+        return res
