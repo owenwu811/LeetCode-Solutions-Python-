c1ed0c4847dfd6ac2622a3d97f53deb2c06cf279
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2025-06-05 21:17:04 -0700

    Update binarysearchtreeiterator.py

--- binarysearchtreeiterator.py
+++ binarysearchtreeiterator.py
@@ -71,4 +71,34 @@ class BSTIterator:
 
     def hasNext(self) -> bool:
         return self.lst
+
+
+
+#my own solution using python3 on 6/5/25:
+
+#do the inorder traversal with a queue in the constructor and then just keep poplefting until you can't anymore
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class BSTIterator:
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+        self.cur = deque()
+        def f(root):
+            if not root:
+                return
+            f(root.left)
+            self.cur.append(root.val)
+            f(root.right)
+        f(root)
+        print(self.cur)
+
+    def next(self) -> int:
+        return self.cur.popleft()
         
+    def hasNext(self) -> bool:
+        return len(self.cur) > 0
