fd769acc0f13fdb28912c85107dad55d1286c6a0
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2025-06-05 21:05:15 -0700

    Update findbottomlefttreevalue.py

--- findbottomlefttreevalue.py
+++ findbottomlefttreevalue.py
@@ -31,3 +31,32 @@ class Solution:
             if level:
                 res.append(level)
         return res[-1][0] #[[2], [1, 3]] > 1, [[1], [2, 3], [4, 5, 6], [7]] > 7
+
+
+#6/5/25 review:
+
+#easy level order bfs
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        d = deque()
+        d.append(root)
+        levels = []
+        while d:
+            level = []
+            for i in range(len(d)):
+                cur = d.popleft()
+                if cur:
+                    level.append(cur.val)
+                    d.append(cur.left)
+                    d.append(cur.right)
+            if level:
+                levels.append(level)
+        print(levels)
+        return levels[-1][0]
